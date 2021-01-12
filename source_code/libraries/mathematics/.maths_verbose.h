1abd6fe2502ef120d86c21984ec565e585456932
cryptoworkbench <nj.vanhoof@gmail.com>
2021-01-12 18:15:01 +0100

    new project layout and makefile improvement

--- source_code/libraries/mathematics/.maths_verbose.h
+++
@@ -1,1 +0,0 @@
-void return_gcd_calculation(unsigned long a, unsigned long b); /* Returns a visual calculation of the Euclidean algorithm used to find the gcd (greatest common divisor) of two numbers */
