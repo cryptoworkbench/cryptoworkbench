3a976c7285b9f76f41cd255b193beb216453c119
cryptoworkbench <nj.vanhoof@gmail.com>
2021-01-12 23:03:14 +0100

    Better organization

---
+++ source_code/libraries/functional/triple_ref_pointers.h
@@ -0,0 +1,7 @@
+/* DESCRIPTION:
+ * These functions were inspired by:
+ * https://www.youtube.com/watch?v=0ZEX_l0DFK0 */
+#include <stdlib.h>
+
+void **phallus();
+void *disintermediate(void **to_be_dereferenced);
