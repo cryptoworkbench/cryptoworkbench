77eafa6dc753a6a854f0ec936c4b5ae0b51c2b77
cryptoworkbench <nj.vanhoof@gmail.com>
2021-01-13 21:43:04 +0100

    almost professional prime factorization

--- source_code/project_code/prime_factorization/prime_factorization.c
+++ source_code/project_code/prime_factorization/prime_factorization.c
@@ -1,68 +1,93 @@
 /* DESCRIPTION:
- * modification of 'lookup_factorize()' in order to use a prime table in order to immediately factorize down to the prime factors.
+ * This is a rather convoluted design.
  *
  * Closes and opens prime_table_fs constantly in order to start reading from the beginning again.
- *
- * I wrote this quickly but I think it is good. I believe it handles all exceptions well.
- * When the prime table is too short, when the factor set is just one prime, when the factor set is multiple primes, etc
  */
 #include <stdio.h>
 #include "../../libraries/mathematics/maths.h"
 #include "../../libraries/functional/string.h"
 #include "../../libraries/functional/triple_ref_pointers.h"
+#define RESET "\x1B[30m"
+#define RED "\x1B[31m"
 
-unsigned long prime_factorize(unsigned long presumed_composite, int binary_mode, char *name) {
-    do {
-	FILE *prime_table_fs;
-	if ( !(prime_table_fs = fopen(name, "r"))) {
-	    fprintf(stderr, "Failed to open prime table\n");
-	    return -1; }
-	// ^^ open prime_table_fs
+struct prime_power {
+    unsigned long prime;
+    unsigned long power;
+    struct prime_power *next;
+};
 
-	unsigned long potential_divisor;
-	while (1) {
-	    if (_read_prime(&potential_divisor, prime_table_fs, binary_mode)) {
-		if (potential_divisor * potential_divisor > presumed_composite) {
-		    fprintf(stdout, "%lu\n", presumed_composite);
-		    return presumed_composite; // successfully factorized
-		} else if (presumed_composite % potential_divisor == 0) {
-		    do {
-			fprintf(stdout,"%lu * ", potential_divisor);
-			presumed_composite = presumed_composite / potential_divisor;
-		    } while (presumed_composite % potential_divisor == 0);
-		    fclose(prime_table_fs);
-		    break; // stop reading prime table from current location and reopen to start reading from the beginning again
-		}
-	    } else {
-		fprintf(stderr, "Prime table too short!\n");
-		return 0; // prime table too short
-	    }
-	}
-    } while (presumed_composite != 1);
+void insert_prime_power(struct prime_power **tracer, unsigned long prime_divisor) {
+    struct prime_power *new_element = (struct prime_power *) malloc(sizeof(struct prime_power));
+    new_element->prime = prime_divisor;
+
+    while (*tracer) // ###==-- Find last insertion --===>
+	tracer = &(*tracer)->next;
+
+    *tracer = new_element; // <===-- And place the location of new_prime into the next field of the previous insertion --==###
 }
+
+void update(struct prime_power **ll_conx, unsigned long new, unsigned long *factor_set) {
+    insert_prime_power(ll_conx, new);
+    *factor_set = *factor_set / new; }
+
+struct prime_power **prime_factorize(struct prime_power **ll_conx, unsigned long factor_set, int binary_mode, char *name) {
+    unsigned long potential_divisor; int read; FILE *table_fs; 
+    do {
+	if (factor_set == 1) { return ll_conx; }
+	// ^^ Base case
+
+	if ( !(table_fs = fopen(name, "r"))) {
+	    fprintf(stderr, "Failed to open!\n");
+	    return ll_conx; }
+	// ^^ Open fs
+
+	while (read = _read_prime(&potential_divisor, table_fs, binary_mode)) {
+	    if (potential_divisor * potential_divisor > factor_set) { update(ll_conx, factor_set, &factor_set); break; }
+	    else if (factor_set % potential_divisor == 0) { do update(ll_conx, potential_divisor, &factor_set); while (factor_set % potential_divisor == 0); break; } }
+	fclose(table_fs);
+    } while (read); fprintf(stderr, "Too short\n"); return ll_conx; }
+
+unsigned long combined_factors(struct prime_power *origin) {
+    unsigned long ret_val = 1;
+    do {
+	fprintf(stdout, "%lu", origin->prime);
+	ret_val *= origin->prime;
+	if (origin->next != NULL)
+	    fprintf(stdout, " * ");
+
+	origin = origin->next;
+    } while (origin != NULL); return ret_val; }
+// ^^ prints and combines the prime factors
 
 int main(int argc, char **argv) {
     if (argc != 3) {
 	fprintf(stderr, "Wrong argument count.\n");
 	return -1;
     } unsigned long factor_set = str_to_ul(argv[1]);
-    unsigned long result = prime_factorize(factor_set, 1, argv[2]);
+    struct prime_power *head;
+    if (!(head = (struct prime_power *) disintermediate((void **) prime_factorize((struct prime_power **) phallus(), factor_set, 1, argv[2])))) {
+	fprintf(stderr, "Factorization failed because there was a problem accessing the neccessary information; namely the first n primes required for factorizing %lu.\n", factor_set);
+	fprintf(stderr, "Where n is from the natural numbers such that:\nn <= %lu^(1/2)\n", factor_set);
+	fprintf(stderr, "\nExiting '-3'.\n");
+	return -3; } unsigned long resulting_set = combined_factors(head);
+    if (resulting_set == factor_set) {
+	fprintf(stdout, " = %lu\n", resulting_set);
+	fprintf(stdout, "\nPrime factorization succesfull.\n");
+	fprintf(stdout, "\nExiting '-0'.\n");
+	return 0;
+    } else {
+	fprintf(stderr, " != %lu\n", factor_set);
+	fprintf(stderr, "\nPrime factorization unsuccesfull.\n");
+	fprintf(stderr, "\nprime table at specified path \"%s\" must be corrupt.\n", argv[1]);
+	fprintf(stderr, "\nExiting '-2'.\n");
+	return -2; }
+    return 0; }
+// ^^ RETURN CODE LEGENDA:
+// 0. Prime factorization successfully completed
+// -1. Wrong argument count.
+// -2. Prime table at specified path \"%s\" must be corrupt:
+//     #1 Put all factors of factor_set which are in the specified prime table's list of the potential factors into the linked list.
+//     #2 And yet all the prime sloths of the linked list elements multiplied together does not add up to the original factor set again.
+//     #3 But 
+// -3. Failed because cannot open the prime table or the prime table is too short
 
-    // The following three are the only possibilities
-    int ret_val;
-    if (result) {
-	if (result == factor_set) { ret_val = 0; }
-	else { ret_val = 1; }
-    } else ret_val = -2; // -1 is already reserved
-
-    switch (ret_val) {
-	case -2:
-	    fprintf(stderr, "Prime table insufficient.\n");
-	    break;
-	case 0:
-	    fprintf(stdout, "%lu is prime.\n", factor_set);
-	    break;
-	case 1:
-	    fprintf(stdout, "%lu is composite.\n", factor_set);
-    } return ret_val; }
-// ^^ End main with this nice switch mechanism
