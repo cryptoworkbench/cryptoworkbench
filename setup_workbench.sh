4475575731c205145293b94ce332506f53bd40e9
cryptoworkbench <nj.vanhoof@gmail.com>
2021-03-23 12:46:47 +0100

    Renamed cayley_tables to cayley_table_generator and more minor improvements to README.md

---
+++ setup_workbench.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+mkdir workbench/
+cd source_code/project_code/general_subgroup_generator && make general_subgroup_generator
+cd ../../../source_code/project_code/cayley_table_generator && make cayley_table_generator
+cd ../../../source_code/project_code/prime_table_generator && make prime_table_generator
