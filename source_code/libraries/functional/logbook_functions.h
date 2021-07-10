b3a0066418bf17ed2fa3f1e98f99589012f9a687
cryptoworkbench <nj.vanhoof@gmail.com>
2021-07-10 22:35:22 +0200

    Changes to logbook functions lib for the better

--- source_code/libraries/functional/logbook_functions.h
+++ source_code/libraries/functional/logbook_functions.h
@@ -12,5 +12,5 @@
 #define LOGBOOK_NAME "../lab_logbook" // <<< This will be the logbook filename
 #define LOGBOOK_FORMULA "%s: "// <<< This will be the name prepend
 
-FILE *open_modular_group_UNRESTRICTED(char *program_name, unsigned long CAP, unsigned long ID, char **location_of_char_pointer_to_filename_in_allocated_memory, FILE **logbook_fs_ptr);
-FILE *open_modular_group(char *program_name, unsigned long CAP, unsigned long ID);
+FILE *open_modular_group_UNRESTRICTED(char *program_name, unsigned long CAP, unsigned long ID, char **location_of_char_pointer_to_filename_in_allocated_memory, FILE *logbook_fs);
+FILE *open_modular_group(FILE *throw_away_logbook_fs, char *program_name, unsigned long CAP, unsigned long ID);
