bb56de37833e0dd25af2c9c0ab20130a481e4b64
cryptoworkbench <nj.vanhoof@gmail.com>
2021-07-30 21:51:05 +0200

    Organized the new awesomeness a little better.

--- source_code/libraries/mathematics/group_operations.c
+++ source_code/libraries/mathematics/group_operations.c
@@ -1,31 +1,26 @@
-/* ############# REMINDERS:
- * ########### #1: "See if I can sort out the lines 7, 8, 11 and 12."
- */
 #include "group_operations.h"
 // ^^^ Required because here I need the definitions of "ADDITIVE_IDENTITY" and "MULTIPLICTIVE_IDENTITY"
 
 unsigned long ordinator(unsigned long a, unsigned long b) { return a + b; }
-unsigned long CAPPED_ordinator(unsigned long a, unsigned long b, unsigned long CAP) { return ordinator(a, b) % CAP; }
-// ^^^ Keep them in this order for now (see "REMINDER #1")
-
 unsigned long ordinated(unsigned long a, unsigned long b) { return a * b; }
-unsigned long CAPPED_ordinated(unsigned long a, unsigned long b, unsigned long CAP) { return ordinated(a, b) % CAP; }
-// ^^^ Keep them in this order for now (see "REMINDER #1")
+// ^^^ Define respectively, the following first-degree (in)finite field combinations:
+// ^^^ ~ Addition in infinite fields (regular addition without MODding the result)
+// ^^^ ~ Addition in finite fields (addition with MODding the result)
 
-function get_applicable_function_for_(unsigned long SP) { if (SP == 0) return &ordinator; else if (SP == 1) return &ordinated; }
-CAPPED_function get_applicable_CAPPED_function_for_(unsigned long SP) { if (SP == 0) return &CAPPED_ordinator; else if (SP == 1) return &CAPPED_ordinated; }
+unsigned long CAPPED_ordinator(unsigned long a, unsigned long b, unsigned long CAP) { return ordinator(a, b) % CAP; }
+unsigned long CAPPED_ordinated(unsigned long a, unsigned long b, unsigned long CAP) { return ordinated(a, b) % CAP; }
+// ^^^ Define respectively, the following second-degree (in)finite field combinations:
+// ^^^ ~ Multiplication in infinite fields (regular multiplication without MODding the result)
+// ^^^ ~ Multiplication in finite fields (multiplication with MODding the result)
+
+function get_applicable_function_for_(unsigned long SP) {
+    if (SP == ADDITIVE_IDENTITY) return &ordinator;
+    else if (SP == MULTIPLICATIVE_IDENTITY) return &ordinated;
+}
+
+CAPPED_function get_applicable_CAPPED_function_for_(unsigned long SP) {
+    if (SP == ADDITIVE_IDENTITY) return &CAPPED_ordinator;
+    else if (SP == MULTIPLICATIVE_IDENTITY) return &CAPPED_ordinated;
+}
 
 unsigned long RESTRICT(function base_UNARY, unsigned long a, unsigned long b, unsigned long CAP) { return base_UNARY(a, b) % CAP; }
-
-/* ############# OLD:
-unsigned long _MODULAR_addition(unsigned long A, unsigned long B, unsigned long CAP) { return (A + B) % CAP; }
-unsigned long _MODULAR_multiplication(unsigned long A, unsigned long B, unsigned long CAP) { return (A * B) % CAP; }
-// ^^^ Functions for the first two modular operations of arithmetica
-
-MODULAR_operation_PTR MODULAR_operation_BASED_ON_(unsigned long ID)
-{   if (ID == ADDITIVE_IDENTITY) return &_MODULAR_addition;
-    else if (ID == MULTIPLICATIVE_IDENTITY) return &_MODULAR_multiplication;
-}
-// ^^ This function returns the right function pointer to one of the two above functions from modular arithmetic
-*/
-
--- source_code/libraries/mathematics/group_operations.h
+++ source_code/libraries/mathematics/group_operations.h
@@ -20,15 +20,3 @@ CAPPED_function get_applicable_CAPPED_function_for_(unsigned long SP);
 
 unsigned long RESTRICT(function base_UNARY, unsigned long a, unsigned long b, unsigned long CAP);
 // ^^^ And this is the magic, see under_development/
-
-/* ############# OLD:
-unsigned long _MODULAR_multiplication(unsigned long A, unsigned long B, unsigned long CAP);
-unsigned long _MODULAR_addition(unsigned long A, unsigned long B, unsigned long CAP);
-// ^^^ Functions for the first two modular operations of arithmetica
-
-typedef unsigned long (*MODULAR_operation_PTR) (unsigned long, unsigned long, unsigned long);
-// ^^ Assigning an "alias"/"nickname", or rather "abbreviation" to this type of function pointer (because otherwise my compiler gets confused).
-
-MODULAR_operation_PTR MODULAR_operation_BASED_ON_(unsigned long ID);
-// ^^ This function returns the right function pointer to one of the two above functions which represent two of the most basic functions that exist in modular arithmetica
-*/
