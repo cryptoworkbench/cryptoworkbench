f0a2cfc3696fc84ab38dc8f7c74466c4122dc5c4
cryptoworkbench <nj.vanhoof@gmail.com>
2021-12-16 11:25:17 +0100

    Significantly modified "SUBGROUP_EXAMPLIFIER.c", it now includes an array_search function as desired.

--- SOURCE_CODE/INSTRUMENTS/SUBGROUP_EXAMPLIFIER/SUBGROUP_EXAMPLIFIER.c
+++ SOURCE_CODE/INSTRUMENTS/SUBGROUP_EXAMPLIFIER/SUBGROUP_EXAMPLIFIER.c
@@ -36,6 +36,47 @@ typedef struct vertibrae {
     struct content unit;
 } array_piece; typedef array_piece *table_type;
 
+unsigned long cardinality;
+table_type LOOKUP_table;
+
+unsigned long read_ul(struct vertibrae *ol) {
+    return ol->unit.literal;
+}
+
+char *read_numerical(struct vertibrae *ol) {
+    return ol->unit.ASCII_numerical;
+}
+
+array_piece *table_partition_search(unsigned long ul) {
+    for (unsigned long iter = 0; iter < cardinality; iter++)
+	if (LOOKUP_table[iter].unit.literal == ul) return &LOOKUP_table[iter];
+	// if (LOOKUP_table[iter].unit.literal == ul) return LOOKUP_table + iter;
+
+    return NULL;
+}
+
+array_piece *table_partition_search_two(unsigned long ul) {
+    void *looper = (void *) LOOKUP_table;
+
+    for (unsigned long i = 0; i < cardinality; i++) {
+	if (read_ul( (array_piece *) looper) == ul) return (array_piece *) looper;
+	else looper += sizeof(struct vertibrae);
+    }
+}
+
+/* This function does seem to work
+array_piece *table_partition_search_three(unsigned long ul) {
+    array_piece *looper = LOOKUP_table;
+
+    unsigned long i = 0;
+    for (i = 0; i < cardinality; i++) {
+	if (looper->unit.literal == ul) return looper;
+	else { looper++; i++; }
+    }
+
+    return NULL;
+} */
+
 void triple_ref_LL_insert(struct triple_ref_LL **tracer, unsigned long new_ulong) {
     struct triple_ref_LL *new_LL_element = (struct triple_ref_LL *) malloc(sizeof(struct triple_ref_LL)); // Fix existence of new pointer_list element
     new_LL_element->next = NULL;
@@ -47,12 +88,12 @@ void triple_ref_LL_insert(struct triple_ref_LL **tracer, unsigned long new_ulong
     *tracer = new_LL_element; // <===-- And place the location of new_LL_element into the next field of the previous insertion --==###
 }
 
-struct triple_ref_LL *establish_LL(char **argv, struct triple_ref_LL **channel, unsigned long *group_cardinality, unsigned long *cell_width, group_OBJ group) {
-    char *path_to_filename; FILE *ELEMENT_database = open_group(argv[0], group, argv[1], &path_to_filename);
+struct triple_ref_LL *establish_LL(char **argv, struct triple_ref_LL **channel, unsigned long *cell_width, group_OBJ group) {
+    char *path_to_filename; FILE *ELEMENT_database = open_group(argv[0], group, argv[1], &path_to_filename); cardinality = 0;
     // ^^^ Open filestream to element database
 
     unsigned long group_ELEMENT;
-    while (fscanf(ELEMENT_database, "%lu\n", &group_ELEMENT) == 1) { triple_ref_LL_insert(channel, group_ELEMENT); (*group_cardinality)++; }
+    while (fscanf(ELEMENT_database, "%lu\n", &group_ELEMENT) == 1) { triple_ref_LL_insert(channel, group_ELEMENT); cardinality++; }
     // ^^^ Establish lineair linked list containing all group elements using the triple ref technique
 
     close_group(argv[1], operation_symbol_from_ID_Sloth(group), path_to_filename, ELEMENT_database);
@@ -69,27 +110,16 @@ struct triple_ref_LL *establish_LL(char **argv, struct triple_ref_LL **channel, 
     return last_element->next;
 }
 
-table_type replace_LL_with_table(struct triple_ref_LL *chain, unsigned long group_cardinality, unsigned long cell_width) {
-    table_type table = (table_type) malloc(sizeof(array_piece) * group_cardinality);
-    /* ^^ Create table of necessitated size ^^ */
+void replace_LL_with_table(struct triple_ref_LL *chain, unsigned long cell_width) {
+    LOOKUP_table = (array_piece *) malloc(sizeof(array_piece) * cardinality);
+    /* ^^ Allocates memory space on the heap for the table. ^^ */
 
     unsigned long index = 0; struct triple_ref_LL *do_loop_iterator = chain; do {
-	struct triple_ref_LL *suspend = do_loop_iterator;
-
-	table[index].unit.literal = suspend->element;
-	table[index].unit.ASCII_numerical = str_from_ul(table[index].unit.literal, cell_width);
-	/* ^^ Fill in unit ^^ */
-
-	do_loop_iterator = suspend->next;
-	
-	printf("Suspending: %lu.\n", suspend->element);
-	free(suspend);
-	
-	index++;
-    } while (do_loop_iterator != chain);
-    /* ^^^ Make an array of the required size. ^^^ */
-
-    return table;
+	struct triple_ref_LL *process = do_loop_iterator;
+	LOOKUP_table[index].unit.literal = process->element;
+	LOOKUP_table[index].unit.ASCII_numerical = str_from_ul(LOOKUP_table[index].unit.literal, cell_width);
+	do_loop_iterator = process->next; free(process); index++;
+    } while (do_loop_iterator != chain); /* << Creates the table. ^^ */
 }
 
 int main(int argc, char **argv) { group_OBJ group;
@@ -105,18 +135,22 @@ int main(int argc, char **argv) { group_OBJ group;
 	    default: if (!boolean_from_ID_Sloth(group)) { shifts->X %= group->MOD; shifts->Y %= group->MOD; } } // << Only applies the mod value to shifts when dealing with additive groups (see "MATH_HINT_ONE")
     }
 
-    unsigned long cell_width; unsigned long group_cardinality = 0;
-    struct triple_ref_LL *identity_element = establish_LL(argv, (struct triple_ref_LL **) sub_ordinator(), &group_cardinality, &cell_width, group);
-    table_type table = replace_LL_with_table(identity_element, group_cardinality, cell_width);
+    unsigned long cell_width; struct triple_ref_LL *identity_element = establish_LL(argv, (struct triple_ref_LL **) sub_ordinator(), &cell_width, group);
+    replace_LL_with_table(identity_element, cell_width);
     
-    printf("Id: %lu\n", table[0].unit.literal);
+    printf("ID: %lu\n", LOOKUP_table[0].unit.literal);
     printf("Cell width: %lu\n", cell_width);
-    printf("group cardinality: %lu\n", group_cardinality);
+    printf("Group cardinality: %lu\n\n\n", cardinality);
 
-    for (unsigned long index = 0; index < group_cardinality; index++) fprintf(stdout, "Element: %s\n", table[index].unit.ASCII_numerical);
+    for (unsigned long index = 0; index < group->MOD; index++) {
+	void *ptr = (void *) table_partition_search(index);
+	if (ptr == NULL) fprintf(stdout, "Element %lu not found.\n", index);
+	else fprintf(stdout, "Element %lu found: %s, %lu\n", index, read_numerical((struct vertibrae *) ptr), read_ul((struct vertibrae *) ptr));
+    }
 
     return 0;
 }
+
 /* MATH HINTS (!):
  * "MATH_HINT_ONE":
  * 	For any finite group G, the order (number of elements) of every subgroup of G divides the order of G. At the very least I know this to be true for additive groups.
