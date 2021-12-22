77210189713992608efd1432a232eec04e45b96a
cryptoworkbench <nj.vanhoof@gmail.com>
2021-12-22 16:28:11 +0100

    Managed to simplify "group_information.c" even further.

--- SOURCE_CODE/libraries/functional/triple_ref_pointers.h
+++ SOURCE_CODE/libraries/functional/triple_ref_pointers.h
@@ -3,5 +3,5 @@
 struct _CHANNEL_PTR_pair { void **head; void **iterator; };
 
 void **open_CHANNEL();
-void INITIALIZE_CHANNEL_PTR_pair(void ***PTR_one, void ***PTR_two);
+void INITIALIZE_CHANNEL_PTR_pair(struct _CHANNEL_PTR_pair *in);
 void *_close_CHANNEL(void **to_be_dereferenced);
