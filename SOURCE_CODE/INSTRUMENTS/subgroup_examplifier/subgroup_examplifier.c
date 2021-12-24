fa1a66c168dcfc5909eee759ff4764ba1226ba8b
cryptoworkbench <nj.vanhoof@gmail.com>
2021-12-24 23:31:03 +0100

    Actually only now it works proper.

--- SOURCE_CODE/INSTRUMENTS/subgroup_examplifier/subgroup_examplifier.c
+++ SOURCE_CODE/INSTRUMENTS/subgroup_examplifier/subgroup_examplifier.c
@@ -2,7 +2,45 @@
  *
  * Works perfectly now. I am very content with it.
  */
-#include "subgroup_examplifier.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../libraries/functional/string.h" // <<< Needed for "match()", "STR_could_be_parsed_into_UL()", etc
+#include "../../libraries/functional/triple_ref_pointers.h" // << Needed for "zoom_out()", "initialize_PTR_pair()", and "zoom_in()"
+#include "../../libraries/mathematics/universal_group_library.h" // <<< Needed for "group_OBJ"
+#define STDOUT_VERTICAL_OFFSET_ERROR "Failed to parse \"%s\" (the 4th argument) as vertical offset. Defaulting to not using a vertical offset.\n"
+#define STDERR_HORIZONTAL_OFFSET_ERROR "Failed to parse \"%s\" (the 3th argument) as horizontal offset. Defaulting to not using a horizontal offset.\n"
+#define STDOUT_ARGV_TWO_INSTRUCTION "Please provide as first argument the modulus of the group in decimal notation.\n"
+#define STDOUT_ARGV_ONE_INSTRUCTION "Okay so the modulus is %s\n\nBut what is this group identity?, what's this group's point of symetry?\n\nIs %s supposed to be the modulus for an additive group?, or is %s supposed to be the modulus for a multiplicative group?\n\n\nTO SPECIFY ADDITIVE (MODULAR) OPERATIONS/ARITHMETIC USE:\n0\n+\naddition\nadditions\nadditive\n\nTO SPECIFY MULTIPLICATIVE (MODULAR) OPERATIONS/ARITHMATIC USE:\n1\n*\nmultiplication\nmultiplications\nmultiplicative\n\n"
+#define HELP_INFORMATION "Program usage: %s <CAP> <ID> [horizontal offset] [vertical offset] [output filename]\n\n<MANDATORY ARGUMENTS> are denoted like this. The program won't run without these.\n\n[optional arguments] are denoted like this. They are not very necessary.\n"
+
+struct offset_values { unsigned long Y; unsigned long X; };
+struct _general_LL { struct _general_LL *next; unsigned long element; };
+typedef struct vertibrae { char *ASCII; unsigned long ulong; unsigned long perm_length; unsigned long *permutation; } array_piece;
+typedef array_piece *table_type; // << Belongs to this one above
+// ^^ Declare variable types
+
+unsigned long cardinality, generator_count;
+_group_operation group_operation;
+table_type LOOKUP_table;
+FILE *main_fs;
+struct offset_values *shifts;
+// ^^ Declare variables
+
+const char *help_queries[] = {"--help", "-h", "help", "instructions", "usage", "--instructions", "--usage", "syntax", "--syntax"};
+// ^^ I think that it is alrigh to put a variable initialization like this in this header file
+
+unsigned long index_lookup(unsigned long ul);
+void print_subgroup(unsigned long index);
+void insert(struct _general_LL ***tracer, unsigned long new_ulong);
+unsigned long *array_from_LL(struct _general_LL **head_TRACER, unsigned long *required_array_size);
+unsigned long *yield_subgroup(unsigned long index, group_OBJ group);
+struct VOID_ptr_ptr_PAIR element_LL_from_file(char **argv, group_OBJ group);
+unsigned long *second_MAIN(struct VOID_ptr_ptr_PAIR, group_OBJ group);
+unsigned long process_generator_array(unsigned long *generator_array, char *modulus, const char *symbol);
+void MOD_not_parsable_ERROR(char *argv_one);
+void ID_not_parsable_ERROR(char *argv_one, char *argv_two);
+void HELP_AND_QUIT(char *prog_NAME);
+// ^^ Function declarations
 
 int main(int argc, char **argv) { group_OBJ group; main_fs = stdout;
     if (6 < argc || argc > 1 && match(argv[1], help_queries)) HELP_AND_QUIT(argv[0]); else group = (group_OBJ) malloc(sizeof(group_OBJ));
@@ -77,6 +115,7 @@ unsigned long *array_from_LL(struct _general_LL **head_TRACER, unsigned long *re
 	iter = process->next; free(process);
     } return ulong_array;
 }
+
 unsigned long *yield_subgroup(unsigned long index, group_OBJ group) {
     unsigned long ID = boolean_from_ID_Sloth(group);
     struct VOID_ptr_ptr_PAIR permutation_LL_pair = INITIALIZE_CHANNEL_PTR_pair();
@@ -133,7 +172,9 @@ unsigned long *second_MAIN(struct VOID_ptr_ptr_PAIR element_CHANNEL_PTR_pair, gr
 
 unsigned long process_generator_array(unsigned long *generator_array, char *modulus, const char *symbol) {
     fprintf(main_fs, "GENERATOR COUNT FOR \u2115%s%s (%lu):\n", modulus, symbol, generator_count);
-    unsigned long i = shifts->Y % generator_count; do { print_subgroup(generator_array[i % generator_count]); if (i < (generator_count + shifts->Y)) { fprintf(main_fs, ", and\n"); i++; } else break; } while (1);
+    unsigned long i = shifts->Y % generator_count; do {
+	print_subgroup(generator_array[i % generator_count]);
+	if (i % generator_count != (shifts->Y - 1) % generator_count) { fprintf(main_fs, ", and\n"); i++; } else break; } while (1);
     fprintf(main_fs, "\n"); free(generator_array); return generator_count;
 }
 
