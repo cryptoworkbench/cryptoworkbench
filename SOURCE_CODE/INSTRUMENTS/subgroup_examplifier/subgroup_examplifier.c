1a0914c314cd2b37b32b30405d50406da9480593
cryptoworkbench <nj.vanhoof@gmail.com>
2021-12-25 09:25:23 +0100

    Finally done with project "subgroup_examplifier" (195 lines of code).

--- SOURCE_CODE/INSTRUMENTS/subgroup_examplifier/subgroup_examplifier.c
+++ SOURCE_CODE/INSTRUMENTS/subgroup_examplifier/subgroup_examplifier.c
@@ -1,7 +1,4 @@
-/* Examplifies additive and multiplicative groups.
- *
- * Works perfectly now. I am very content with it.
- */
+// Examplifies additive and multiplicative groups and lists their generators.
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../libraries/functional/string.h" // <<< Needed for "match()", "STR_could_be_parsed_into_UL()", etc
@@ -29,6 +26,7 @@ struct offset_values *shifts;
 const char *help_queries[] = {"--help", "-h", "help", "instructions", "usage", "--instructions", "--usage", "syntax", "--syntax"};
 // ^^ I think that it is alrigh to put a variable initialization like this in this header file
 
+/*
 unsigned long index_lookup(unsigned long ul);
 void print_subgroup(unsigned long index);
 void insert(struct _general_LL ***tracer, unsigned long new_ulong);
@@ -40,58 +38,24 @@ unsigned long process_generator_array(unsigned long *generator_array, char *modu
 void MOD_not_parsable_ERROR(char *argv_one);
 void ID_not_parsable_ERROR(char *argv_one, char *argv_two);
 void HELP_AND_QUIT(char *prog_NAME);
+*/
 // ^^ Function declarations
 
-int main(int argc, char **argv) { group_OBJ group; main_fs = stdout;
-    if (6 < argc || argc > 1 && match(argv[1], help_queries)) HELP_AND_QUIT(argv[0]); else group = (group_OBJ) malloc(sizeof(group_OBJ));
-    if (2 > argc || !STR_could_be_parsed_into_UL(argv[1], &group->MOD)) MOD_not_parsable_ERROR(argv[1]);
-    if (3 > argc || !STR_could_be_parsed_into_enum_GROUP_IDentity(argv[2], &group->ID)) ID_not_parsable_ERROR(argv[1], argv[2]);
-    else group_operation = operation_from_ID(group->ID); // <^^^ Parses and processes everything that has to do with CMD args, also deals with the "help_queries"
-
-    shifts = (struct offset_values *) malloc(sizeof(struct offset_values)); shifts->Y = shifts->X = 0;
-    if (argc != 3) { switch (argc) { case 6: main_fs = fopen(argv[5], "w");
-	    case 5: if (!STR_could_be_parsed_into_UL(argv[4], &shifts->Y)) fprintf(stderr, STDOUT_VERTICAL_OFFSET_ERROR, argv[4]);
-	    case 4: if (!STR_could_be_parsed_into_UL(argv[3], &shifts->X)) fprintf(stderr, STDERR_HORIZONTAL_OFFSET_ERROR, argv[3]);
-	    default: if (!boolean_from_ID_Sloth(group)) { shifts->X %= group->MOD; shifts->Y %= group->MOD; } } // << Only applies the mod value to shifts when dealing with additive groups (see "MATH_HINT_ONE")
-    } // ^ Process offset values.
-
-    unsigned long *generator_array = second_MAIN(element_LL_from_file(argv, group), group); // << Check "subgroup_information.h" for elaboration concerning "second_MAIN()"
-    for (unsigned long i = shifts->Y; i < cardinality + shifts->Y; i++) { print_subgroup(i % cardinality); fprintf(main_fs, "\n"); } // << Print table to "main_fs"
-
-    const char *adjective = adjective_from_ID_Sloth(group);
-    const char *symbol = operation_symbol_from_ID_Sloth(group);
-    if (main_fs != stdout) { fclose(main_fs); main_fs = stdout;
-	fprintf(main_fs, "Wrote table for the %s group of integers modulo %s to the external file '%s'\n", adjective, argv[1], argv[5]);
-	fprintf(main_fs, "Horizontal offset used: %lu\nVertical offset used: %lu\n", shifts->X, shifts->Y); }
-    // ^^^ Only the table is supposed to be written to the external file
-
-    fprintf(main_fs, "\n\u2115%s%s contains %lu elements a.k.a. |\u2115%s%s| = %lu\n\n", argv[1], symbol, cardinality, argv[1], symbol, cardinality);
-    // ^^^ Print cardinality information about this group.
-
-    if (generator_array) fprintf(main_fs, "\n\u2191 those are the only %lu generators present in \u2115%s%s \u2191\n", process_generator_array(generator_array, argv[1], symbol), argv[1], symbol);
-    else fprintf(main_fs, "This group does not contain any generators.\n");
-    // ^^^ Calls the amount of generators to the user and recounts it afterwards, horizontal "shifts->X" is used here, check that out. Also free()'s the array
-
-    for (unsigned long index = 0; index < cardinality; index++) { free(LOOKUP_table[index].permutation); free(LOOKUP_table[index].ASCII); } free(LOOKUP_table);
-    // ^^^ Free all of the sloths of memory referred to (in)directly by the table
-
-    return 0;
+void HELP_AND_QUIT(char *prog_NAME) {
+    fprintf(stderr, HELP_INFORMATION, prog_NAME);
+    exit(0);
 }
 
-unsigned long index_lookup(unsigned long ul) {
-    for (unsigned long index = 0; index < cardinality; index++)
-	if (LOOKUP_table[index].ulong == ul) return index;
-
-    return 0;
+void MOD_not_parsable_ERROR(char *argv_one) {
+    fprintf(stdout, STDOUT_ARGV_TWO_INSTRUCTION);
+    fprintf(stderr, "\nFATAL ERROR: cannot grasp infinite field CAP: to attempt to open from ARCHIVE/ the group '\u2115%s*' makes no sense to me. Returning '-1'.\n", argv_one);
+    exit(-1);
 }
 
-void print_subgroup(unsigned long index) {
-    fprintf(main_fs, "<%s> = {", LOOKUP_table[index].ASCII);
-    unsigned long i = 0; do {
-	fprintf(main_fs, "%s", LOOKUP_table[LOOKUP_table[index].permutation[(i + shifts->X) % LOOKUP_table[index].perm_length]].ASCII); i++;
-	if (i == LOOKUP_table[index].perm_length) break;
-	else fprintf(main_fs, ", ");
-    } while (1); fprintf(main_fs, "}");
+void ID_not_parsable_ERROR(char *argv_one, char *argv_two) {
+    fprintf(stdout, STDOUT_ARGV_ONE_INSTRUCTION, argv_one, argv_one, argv_one, argv_two);
+    fprintf(stderr, "\nFATAL ERROR: cannot grasp group ID: '%s' is not in the first list nor in the second. Returning '-2'.\n", argv_two);
+    exit(-2);
 }
 
 void insert(struct _general_LL ***tracer_location, unsigned long new_ulong) {
@@ -105,6 +69,30 @@ void insert(struct _general_LL ***tracer_location, unsigned long new_ulong) {
     
     *tracer_location = (struct _general_LL **) **tracer_location;
     /* And move onto this newly added spot ^^. */
+}
+
+struct VOID_ptr_ptr_PAIR element_LL_from_file(char **argv, group_OBJ group) {
+    cardinality = 0;
+    FILE *ELEMENT_database = open_group(argv[0], group, argv[1]);
+    // ^^^ Open filestream to element database and initialize cardinality counter. ^^
+
+    struct VOID_ptr_ptr_PAIR element_CHANNEL_PTR_pair = INITIALIZE_CHANNEL_PTR_pair();
+    // ^^^ Keep an eye of the head of the open linked list that "insert()" will create. ^^
+
+    unsigned long group_ELEMENT; while (fscanf(ELEMENT_database, "%lu\n", &group_ELEMENT) == 1) { insert((struct _general_LL ***) &element_CHANNEL_PTR_pair.iterator, group_ELEMENT); cardinality++; }
+    // ^^^ Manifest open linked list consisting of all this "group"'s elements using "insert()" (this linked list can only be closed performing "circular_LL_from_CHAN(element_CHANNEL_PTR_pair.head)"). ^^
+
+    close_group(argv[1], operation_symbol_from_ID_Sloth(group), ELEMENT_database);
+    // ^^^ After successfull interpretation from element_database, notify of the file's parsing in the logbook
+
+    return element_CHANNEL_PTR_pair;
+}
+
+unsigned long index_lookup(unsigned long ul) {
+    for (unsigned long index = 0; index < cardinality; index++)
+	if (LOOKUP_table[index].ulong == ul) return index;
+
+    return 0;
 }
 
 unsigned long *array_from_LL(struct _general_LL **head_TRACER, unsigned long *required_array_size) {
@@ -130,23 +118,6 @@ unsigned long *yield_subgroup(unsigned long index, group_OBJ group) {
     return array_from_LL((struct _general_LL **) permutation_LL_pair.head, &subgroup_card);
 }
 
-struct VOID_ptr_ptr_PAIR element_LL_from_file(char **argv, group_OBJ group) {
-    cardinality = 0;
-    FILE *ELEMENT_database = open_group(argv[0], group, argv[1]);
-    // ^^^ Open filestream to element database and initialize cardinality counter. ^^
-
-    struct VOID_ptr_ptr_PAIR element_CHANNEL_PTR_pair = INITIALIZE_CHANNEL_PTR_pair();
-    // ^^^ Keep an eye of the head of the open linked list that "insert()" will create. ^^
-
-    unsigned long group_ELEMENT; while (fscanf(ELEMENT_database, "%lu\n", &group_ELEMENT) == 1) { insert((struct _general_LL ***) &element_CHANNEL_PTR_pair.iterator, group_ELEMENT); cardinality++; }
-    // ^^^ Manifest open linked list consisting of all this "group"'s elements using "insert()" (this linked list can only be closed performing "circular_LL_from_CHAN(element_CHANNEL_PTR_pair.head)"). ^^
-
-    close_group(argv[1], operation_symbol_from_ID_Sloth(group), ELEMENT_database);
-    // ^^^ After successfull interpretation from element_database, notify of the file's parsing in the logbook
-
-    return element_CHANNEL_PTR_pair;
-}
-
 unsigned long *second_MAIN(struct VOID_ptr_ptr_PAIR element_CHANNEL_PTR_pair, group_OBJ group) {
     struct _general_LL *LINEAR_element_LL;
     if (!(LINEAR_element_LL = (struct _general_LL *) _close_CHANNEL(element_CHANNEL_PTR_pair.head))) { fprintf(stderr, "Failed to add elements from 'ARCHIVE/' file. Exiting '-11'.\n"); exit(-11); }
@@ -170,6 +141,15 @@ unsigned long *second_MAIN(struct VOID_ptr_ptr_PAIR element_CHANNEL_PTR_pair, gr
     return array_from_LL((struct _general_LL **) generator_LL_pair.head, &generator_count);
 }
 
+void print_subgroup(unsigned long index) {
+    fprintf(main_fs, "<%s> = {", LOOKUP_table[index].ASCII);
+    unsigned long i = 0; do {
+	fprintf(main_fs, "%s", LOOKUP_table[LOOKUP_table[index].permutation[(i + shifts->X) % LOOKUP_table[index].perm_length]].ASCII); i++;
+	if (i == LOOKUP_table[index].perm_length) break;
+	else fprintf(main_fs, ", ");
+    } while (1); fprintf(main_fs, "}");
+}
+
 unsigned long process_generator_array(unsigned long *generator_array, char *modulus, const char *symbol) {
     fprintf(main_fs, "GENERATOR COUNT FOR \u2115%s%s (%lu):\n", modulus, symbol, generator_count);
     unsigned long i = shifts->Y % generator_count; do {
@@ -178,23 +158,38 @@ unsigned long process_generator_array(unsigned long *generator_array, char *modu
     fprintf(main_fs, "\n"); free(generator_array); return generator_count;
 }
 
-void ID_not_parsable_ERROR(char *argv_one, char *argv_two) {
-    fprintf(stdout, STDOUT_ARGV_ONE_INSTRUCTION, argv_one, argv_one, argv_one, argv_two);
-    fprintf(stderr, "\nFATAL ERROR: cannot grasp group ID: '%s' is not in the first list nor in the second. Returning '-2'.\n", argv_two);
-    exit(-2);
-}
+int main(int argc, char **argv) { group_OBJ group; main_fs = stdout;
+    if (6 < argc || argc > 1 && match(argv[1], help_queries)) HELP_AND_QUIT(argv[0]); else group = (group_OBJ) malloc(sizeof(group_OBJ));
+    if (2 > argc || !STR_could_be_parsed_into_UL(argv[1], &group->MOD)) MOD_not_parsable_ERROR(argv[1]);
+    if (3 > argc || !STR_could_be_parsed_into_enum_GROUP_IDentity(argv[2], &group->ID)) ID_not_parsable_ERROR(argv[1], argv[2]);
+    else group_operation = operation_from_ID(group->ID); // <^^^ Parses and processes everything that has to do with CMD args, also deals with the "help_queries"
 
-void MOD_not_parsable_ERROR(char *argv_one) {
-    fprintf(stdout, STDOUT_ARGV_TWO_INSTRUCTION);
-    fprintf(stderr, "\nFATAL ERROR: cannot grasp infinite field CAP: to attempt to open from ARCHIVE/ the group '\u2115%s*' makes no sense to me. Returning '-1'.\n", argv_one);
-    exit(-1);
-}
+    shifts = (struct offset_values *) malloc(sizeof(struct offset_values)); shifts->Y = shifts->X = 0;
+    if (argc != 3) { switch (argc) { case 6: main_fs = fopen(argv[5], "w");
+	    case 5: if (!STR_could_be_parsed_into_UL(argv[4], &shifts->Y)) fprintf(stderr, STDOUT_VERTICAL_OFFSET_ERROR, argv[4]);
+	    case 4: if (!STR_could_be_parsed_into_UL(argv[3], &shifts->X)) fprintf(stderr, STDERR_HORIZONTAL_OFFSET_ERROR, argv[3]);
+	    default: if (!boolean_from_ID_Sloth(group)) { shifts->X %= group->MOD; shifts->Y %= group->MOD; } } // << Only applies the modulus value to shifts when dealing with additive groups
+    } // ^ Process offset values.
 
-void HELP_AND_QUIT(char *prog_NAME) {
-    fprintf(stderr, HELP_INFORMATION, prog_NAME);
-    exit(0);
+    unsigned long *generator_array = second_MAIN(element_LL_from_file(argv, group), group); // << Check "subgroup_information.h" for elaboration concerning "second_MAIN()"
+    for (unsigned long i = shifts->Y; i < cardinality + shifts->Y; i++) { print_subgroup(i % cardinality); fprintf(main_fs, "\n"); } // << Print table to "main_fs"
+
+    const char *adjective = adjective_from_ID_Sloth(group);
+    const char *symbol = operation_symbol_from_ID_Sloth(group);
+    if (main_fs != stdout) { fclose(main_fs); main_fs = stdout;
+	fprintf(main_fs, "Wrote table for the %s group of integers modulo %s to the external file '%s'\n", adjective, argv[1], argv[5]);
+	fprintf(main_fs, "Horizontal offset used: %lu\nVertical offset used: %lu\n", shifts->X, shifts->Y); }
+    // ^^^ Only the table is supposed to be written to the external file
+
+    fprintf(main_fs, "\n\u2115%s%s contains %lu elements a.k.a. |\u2115%s%s| = %lu\n\n", argv[1], symbol, cardinality, argv[1], symbol, cardinality);
+    // ^^^ Print cardinality information about this group.
+
+    if (generator_array) fprintf(main_fs, "\n\u2191 those are the only %lu generators present in \u2115%s%s \u2191\n", process_generator_array(generator_array, argv[1], symbol), argv[1], symbol);
+    else fprintf(main_fs, "This group does not contain any generators.\n");
+    // ^^^ Calls the amount of generators to the user and recounts it afterwards, horizontal "shifts->X" is used here, check that out. Also free()'s the array
+
+    for (unsigned long index = 0; index < cardinality; index++) { free(LOOKUP_table[index].permutation); free(LOOKUP_table[index].ASCII); } free(LOOKUP_table);
+    // ^^^ Free all of the sloths of memory referred to (in)directly by the table
+
+    return 0;
 }
-/* MATH HINTS (!):
- * "MATH_HINT_ONE":
- * 	For any finite group G, the order (number of elements) of every subgroup of G divides the order of G. This is applicable to additive groups here.
- * 	*/
