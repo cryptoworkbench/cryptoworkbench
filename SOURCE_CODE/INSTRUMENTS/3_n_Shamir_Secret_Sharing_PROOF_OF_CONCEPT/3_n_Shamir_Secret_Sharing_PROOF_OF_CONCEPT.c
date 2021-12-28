8dc720a26be5af14c04aaab7529e5c851e743484
cryptoworkbench <nj.vanhoof@gmail.com>
2021-12-28 19:55:58 +0100

    Got a 3_n_Shamir_Secret_Sharing_Sceme_PROOF_OF_CONCEPT and I've got a 2_n_Shamir_Secret_Sharing_Sceme_PROOF_OF_CONCEPT.

--- SOURCE_CODE/INSTRUMENTS/3_n_Shamir_Secret_Sharing_PROOF_OF_CONCEPT/3_n_Shamir_Secret_Sharing_PROOF_OF_CONCEPT.c
+++ SOURCE_CODE/INSTRUMENTS/3_n_Shamir_Secret_Sharing_PROOF_OF_CONCEPT/3_n_Shamir_Secret_Sharing_PROOF_OF_CONCEPT.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include "../../libraries/functional/string.h"
 
-unsigned long Mod, A, B, C;
+unsigned long m, a, b, c;
 
 struct linear_equation {
     unsigned long coefficient_a;
@@ -31,37 +31,36 @@ unsigned long least_common_multiple(unsigned long a, unsigned long b) {
 
 struct linear_equation multiply_values_by(struct linear_equation INP, unsigned long multiplier) {
     struct linear_equation return_value;
-    return_value.coefficient_a = (INP.coefficient_a * multiplier) % Mod;
-    return_value.coefficient_b = (INP.coefficient_b * multiplier) % Mod;
-    return_value.result = (INP.result * multiplier) % Mod;
+    return_value.coefficient_a = (INP.coefficient_a * multiplier) % m;
+    return_value.coefficient_b = (INP.coefficient_b * multiplier) % m;
+    return_value.result = (INP.result * multiplier) % m;
     return return_value;
 }
 
 struct linear_equation linear_equation_add(struct linear_equation equation_one, struct linear_equation equation_two) {
     struct linear_equation ret;
-    ret.coefficient_a = (equation_one.coefficient_a + (Mod - equation_two.coefficient_a)) % Mod;
-    ret.coefficient_b = (equation_one.coefficient_b + (Mod - equation_two.coefficient_b)) % Mod;
-    ret.result = (equation_one.result + (Mod - equation_two.result)) % Mod;
+    ret.coefficient_a = (equation_one.coefficient_a + (m - equation_two.coefficient_a)) % m;
+    ret.coefficient_b = (equation_one.coefficient_b + (m - equation_two.coefficient_b)) % m;
+    ret.result = (equation_one.result + (m - equation_two.result)) % m;
     return ret;
 }
 
-unsigned long md(unsigned long IN) { return IN % Mod; }
-void C_reduce() { if (C >= Mod) { md(C); printf("The secret has been reduced by mod %lu into the congruent secret %lu.\n\n", Mod, C); } }
+void C_reduce() { if (c >= m) { c%=m; printf("The secret has been reduced by mod %lu into the congruent secret %lu.\n\n", m, c); } }
 
 int main(int argc, char **argv) {
-    if (2 > argc || !STR_could_be_parsed_into_UL(argv[1], &Mod)) { printf("%s is not Mod!\n", argv[1]); exit(-1); }
-    if (3 > argc || !STR_could_be_parsed_into_UL(argv[2], &A)) { printf("%s is not A!\n", argv[2]); exit(-2); } md(A);
-    if (4 > argc || !STR_could_be_parsed_into_UL(argv[3], &B)) { printf("%s is not B!\n", argv[3]); exit(-3); } md(B);
+    if (2 > argc || !STR_could_be_parsed_into_UL(argv[1], &m)) { printf("%s is not m!\n", argv[1]); exit(-1); }
+    if (3 > argc || !STR_could_be_parsed_into_UL(argv[2], &a)) { printf("%s is not a!\n", argv[2]); exit(-2); } a%=m;
+    if (4 > argc || !STR_could_be_parsed_into_UL(argv[3], &b)) { printf("%s is not b!\n", argv[3]); exit(-3); } b%=m;
     if (4 < argc) {
-	if (STR_could_be_parsed_into_UL(argv[4], &C)) C_reduce();
+	if (STR_could_be_parsed_into_UL(argv[4], &c)) C_reduce();
 	else { printf("The secret '%s' has not yet been transformed into numerical form!\n", argv[4]); exit(-4); }
-    } else { printf("Please put in the secret number such that 'secret number' < %lu: ", Mod); fscanf(stdin, "%lu", &C); C_reduce(); }
+    } else { printf("Please put in the secret number such that 'secret number' < %lu: ", m); fscanf(stdin, "%lu", &c); C_reduce(); }
     // ^^ Gather starting information
 
-    fprintf(stdout, "[x,y] points on 'y \u2261 %lux^2 + %lux + %lu (mod %lu)': (complete)\n", A, B, C, Mod);
-    for (unsigned long x = 0; x < Mod; x++) {
+    fprintf(stdout, "[x,y] points on 'y \u2261 %lux^2 + %lux + %lu (mod %lu)': (complete)\n", a, b, c, m);
+    for (unsigned long x = 0; x < m; x++) {
 	struct cartesian_coordinates point_on_graph = {x, 0};
-	unsigned long y = (md(md((md(A * (md(x * x)))) + (md(x * B)))) + C);
+	unsigned long y = (((((((x * x) % m) * a) % m) + ((x * b) % m)) % m) + c) % m;
 	printf("[%lu,%lu]\n", x, y);
     } fprintf(stdout, "\n"); // << ^ Calculate all coordinates for this parabola over this finite field
 
@@ -70,9 +69,9 @@ int main(int argc, char **argv) {
     fprintf(stdout, "Point two: ["); fscanf(stdin, " %lu,%lu]", &point_two.x, &point_two.y);
     fprintf(stdout, "Point three: ["); fscanf(stdin, " %lu,%lu]", &point_three.x, &point_three.y);
 
-    struct linear_equation equation_one = { md(point_one.x * point_one.x), point_one.x, point_one.y };
-    struct linear_equation equation_two = { md(point_two.x * point_two.x), point_two.x, point_two.y };
-    struct linear_equation equation_three = { md(point_three.x * point_three.x), point_three.x, point_three.y };
+    struct linear_equation equation_one = {(point_one.x*point_one.x)%m, point_one.x, point_one.y};
+    struct linear_equation equation_two = {(point_two.x*point_two.x)%m, point_two.x, point_two.y};
+    struct linear_equation equation_three = {(point_three.x*point_three.x)%m, point_three.x, point_three.y};
     struct linear_equation equation_two_and_one = linear_equation_add(equation_two, equation_one);
     struct linear_equation equation_two_and_three = linear_equation_add(equation_two, equation_three);
     unsigned long coefficient_b_lcm = least_common_multiple(equation_two_and_one.coefficient_b, equation_two_and_three.coefficient_b); // printf("\nCalculated lcm as: %lu\n", coefficient_b_lcm);
@@ -81,20 +80,22 @@ int main(int argc, char **argv) {
     struct linear_equation final_linear_equation = linear_equation_add(modified_two_and_one, modified_two_and_three);
     // ^^ Prepare all the required linear equations
 
-    unsigned pre_A = final_linear_equation.result; while (pre_A % final_linear_equation.coefficient_a != 0) pre_A += Mod;
-    unsigned long retrieved_A = pre_A / final_linear_equation.coefficient_a; md(retrieved_A); // << Check if may be ommited
-    if (retrieved_A != A) // return -5 if the quadratic formula parameter 'a' had been incorrectly inferred from the 3 coordinate points (the 3-N in this k = 3 3-N Shamir Secret Sharing Sceme)
-    { fprintf(stderr, "\nThe value that I inferred from coordinate points for variable 'a' did not correspond to the variable 'a' that was used to plot the points. %lu != %lu. Exiting.\n", retrieved_A, A); return -5; }
-    // ^^ Prove that A can be successfully derived from these linear equations by not returning here
+    // ... continuation of program :
+    unsigned division_intermediary;
 
-    unsigned long pre_B = md(equation_two_and_one.result + (Mod - md(equation_two_and_one.coefficient_a * retrieved_A)));
-    while (pre_B % equation_two_and_one.coefficient_b != 0) pre_B += Mod;
-    unsigned long retrieved_B = pre_B / equation_two_and_one.coefficient_b; md(retrieved_B);
-    if (retrieved_B != B) // return -6 is the quadratic formula parameter 'b' had been incorrectly inferred from the 3 coordinate points
-    { fprintf(stderr, "The value that I inferred from coordinate points for variable 'b' did not correspond to the variable 'b' that was used to plot the points. %lu != %lu. Exiting.\n", retrieved_B, B); return -6; }
-    // ^^ Prove that B can be successfully derived from these linear equations by not returning here either
+    division_intermediary = final_linear_equation.result; while (division_intermediary % final_linear_equation.coefficient_a != 0) division_intermediary += m;
+    unsigned long retrieved_a = division_intermediary / final_linear_equation.coefficient_a; retrieved_a %= m; // << Check if may be ommited
+    if (retrieved_a != a) // return -5 if the quadratic formula parameter 'a' had been incorrectly inferred from the 3 coordinate points (the 3-N in this k = 3 3-N Shamir Secret Sharing Sceme)
+    { fprintf(stderr, "\nThe value that I inferred from coordinate points for variable 'a' did not correspond to the variable 'a' that was used to plot the points. %lu != %lu. Exiting.\n", retrieved_a, a); return -5; }
+    // ^^ Prove that a can be successfully derived from these linear equations by not returning here
 
-    unsigned long retrieved_C = md(equation_one.result + (Mod - md(md(md(A * (md(point_one.x * point_one.x)))) + (md(retrieved_B * point_one.x)))));
-    if (C != retrieved_C) { fprintf(stderr, "%lu != %lu\n", retrieved_C, C); return -7; }
+    division_intermediary = (equation_two_and_one.result+(m-((equation_two_and_one.coefficient_a*retrieved_a)%m)))%m; while (division_intermediary % equation_two_and_one.coefficient_b != 0) division_intermediary += m;
+    unsigned long retrieved_b = division_intermediary / equation_two_and_one.coefficient_b; retrieved_b %= m;
+    if (retrieved_b != b) // return -6 is the quadratic formula parameter 'b' had been incorrectly inferred from the 3 coordinate points
+    { fprintf(stderr, "The value that I inferred from coordinate points for variable 'b' did not correspond to the variable 'b' that was used to plot the points. %lu != %lu. Exiting.\n", retrieved_b, b); return -6; }
+    // ^^ Prove that b can be successfully derived from these linear equations by not returning here either
+
+    unsigned long retrieved_C = (equation_one.result + (m - (((retrieved_b * point_one.x) % m ) + ((((point_one.x * point_one.x) % m ) * a) % m )) % m )) % m;
+    if (c != retrieved_C) { fprintf(stderr, "%lu != %lu\n", retrieved_C, c); return -7; }
     else fprintf(stdout, "\nSecret curve parameter value: %lu.\n", retrieved_C); return 0;
 }
