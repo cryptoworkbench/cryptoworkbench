b7592432bd0606452e447a9b33841dc0010f0b0a
cryptoworkbench <nj.vanhoof@gmail.com>
2021-12-31 18:16:57 +0100

    Last work on "elliptic_curve_DH.c" for this year.

--- SOURCE_CODE/INSTRUMENTS/elliptic_curve_DH/elliptic_curve_DH.c
+++ SOURCE_CODE/INSTRUMENTS/elliptic_curve_DH/elliptic_curve_DH.c
@@ -1,4 +1,13 @@
-/* Inspired by: https://www.youtube.com/watch?v=F3zzNa42-tQ */
+/* Inspired by: https://www.youtube.com/watch?v=F3zzNa42-tQ
+ *
+ * Try with 17 2 2 19 5 1
+ * 3, 8
+ *
+ * To get point of infinity.
+ *
+ * For some reason fails when using these arguments in combination with secret values 4, and 6.
+ *
+ * And they do not correspond when you put in 5 with 6. (?). */
 #include <stdio.h>
 #include "../../libraries/functional/string.h"
 #include "../../libraries/mathematics/maths.h"
@@ -25,14 +34,13 @@ void FORMAL_print_point(struct coordinates *point) {
 }
 
 // * Performs both point doubling and point addition
-void point_addition(struct coordinates *p, struct coordinates *q, struct coordinates **r) {
-    if (!(p && q)) { if (p) *r = p; else if (q) *r = q; else *r = NULL; return; } // < return point when combining with ID
-    else if ((p->y != q->y || p->x == 0) && p->x == q->x) { *r = NULL; return; } // < return ID upon adding inverses and upon doubling points with x = 0
-    *r = (struct coordinates *) malloc(sizeof(struct coordinates)); unsigned long s;
-    if (p->x == q->x && p->y == q->y) s = modular_division(((3 * ((p->x * p->x) % m) % m) + a) % m, (2 * p->y) % m); // See math note "#2"
+struct coordinates *point_addition(struct coordinates *p, struct coordinates *q) {
+    if (!(p && q)) { if (p) return p; else if (q) return q; return NULL; } // < return point when combining with ID
+    else if ((p->y != q->y || p->x == 0) && p->x == q->x) { return NULL; } // < return ID upon adding inverses and upon doubling points with x = 0
+    struct coordinates *r = (struct coordinates *) malloc(sizeof(struct coordinates)); unsigned long s;
+    if (p->x == q->x && p->y == q->y) s = modular_division(((3 * ((p->x * p->x) % m) % m) + a) % m, (2 * p->y) % m);
     else s = modular_division(((p->y + (m - q->y)) % m), ((p->x + (m - q->x)) % m));
-    (*r)->x = (((s * s) % m) + inv((p->x + q->x) % m)) % m;
-    (*r)->y = (((s * ((p->x + inv((*r)->x)) % m)) % m) + inv(p->y)) % m;
+    r->x = (((s * s) % m) + inv((p->x + q->x) % m)) % m; r->y = (((s * ((p->x + inv(r->x)) % m)) % m) + inv(p->y)) % m; return r;
 }
 
 void argv_ERROR(unsigned long index, char **argv) { fprintf(stderr, "'%s' not interpretable as %s.\n", argv[index], symbol[index - 1]); exit(-index); }
@@ -48,25 +56,31 @@ void take_in_point(char symbol, struct coordinates **point) {
     }; free(inp);
 }
 
-void point_multiplication(unsigned long multiplier, struct coordinates *base, struct coordinates **result) {
+struct coordinates *point_multiplication(unsigned long multiplier, struct coordinates *base) {
     unsigned long down_rounded_base_two_log = down_rounded_BASE_2_logarithm(multiplier);
     struct coordinates **scalar_multiplication_backbone = (struct coordinates **) malloc(sizeof(struct coordinates *) * (down_rounded_base_two_log + 1));
     *scalar_multiplication_backbone = (struct coordinates *) malloc(sizeof(struct coordinates)); *scalar_multiplication_backbone = base;
-    unsigned long index = 0; do { point_addition(scalar_multiplication_backbone[index], scalar_multiplication_backbone[index], &scalar_multiplication_backbone[index + 1]); index++; } while (index < down_rounded_base_two_log);
+    unsigned long index = 0; do { scalar_multiplication_backbone[index + 1] = point_addition(scalar_multiplication_backbone[index], scalar_multiplication_backbone[index]); index++; } while (index < down_rounded_base_two_log);
     // ^^ Initialize "scalar_multiplication_backbone"
 
-    *result = NULL; // << First set the return value to the identity element (which is the point at infinity, which this program understand as a "NULL" pointer where a "struct coordinates" pointer was expected)
+    struct coordinates *permutation_accumulator = NULL;
+    // ^^ First set the return value to the identity element (which is the point at infinity, which this program understand as a "NULL" pointer where a "struct coordinates" pointer was expected)
+
     if (multiplier != 0) {
 	unsigned long down_rounded_base_two_log = down_rounded_BASE_2_logarithm(multiplier);
 	while (multiplier != 0) {
-	    point_addition(scalar_multiplication_backbone[down_rounded_base_two_log], *result, result);
+	    permutation_accumulator = point_addition(scalar_multiplication_backbone[down_rounded_base_two_log], permutation_accumulator);
 	    multiplier -= N_exponentiation(2, down_rounded_base_two_log);
 	    down_rounded_base_two_log = down_rounded_BASE_2_logarithm(multiplier);
 	}
     } free(scalar_multiplication_backbone);
+    return permutation_accumulator;
 }
 
 void print_multiple_of_ECC_point(unsigned long multiplier, struct coordinates *result) { fprintf(stdout, "%luG = ", multiplier); FORMAL_print_point(result); }
+
+struct coordinates *entire_calculation(unsigned long multiplier, struct coordinates *base)
+{ struct coordinates *i = point_multiplication(multiplier, base); fprintf(stdout, "%lu * (%lu,%lu) = (%lu,%lu)\n", multiplier, base->x, base->y, i->x, i->y); return i; }
 
 int main(int argc, char **argv) {
     if (2 > argc || !STR_could_be_parsed_into_UL(argv[1], &m)) argv_ERROR(1, argv);
@@ -85,28 +99,24 @@ int main(int argc, char **argv) {
     fprintf(stdout, "base point (a.k.a. 'G') (x,y): (%lu,%lu)\n\n", _base_Point.x, _base_Point.y);
     // ^^ Display success
 
-    unsigned long multiplier_Bob;
-    fprintf(stdout, "\nBob's secret value: "); fscanf(stdin, "%lu", &multiplier_Bob);
-    struct coordinates *public_Bob; point_multiplication(multiplier_Bob, &_base_Point, &public_Bob);
-    fprintf(stdout, "Bob's calculation: "); print_multiple_of_ECC_point(multiplier_Bob, public_Bob);
+    unsigned long multiplier_Bob; fprintf(stdout, "Bob's secret value: "); fscanf(stdin, "%lu", &multiplier_Bob);
+    fprintf(stdout, "Bob's calculation : ");  struct coordinates *public_Bob = entire_calculation(multiplier_Bob, &_base_Point);
 
-    unsigned long multiplier_Alice;
-    fprintf(stdout, "\nAlice's secret value: "); fscanf(stdin, "%lu", &multiplier_Alice);
-    struct coordinates *public_Alice; point_multiplication(multiplier_Alice, &_base_Point, &public_Alice);
-    fprintf(stdout, "Alice's calculation: "); print_multiple_of_ECC_point(multiplier_Alice, public_Alice);
+    unsigned long multiplier_Alice; fprintf(stdout, "\nAlice's secret value: "); fscanf(stdin, "%lu", &multiplier_Alice);
+    fprintf(stdout, "Alice's calculation : "); struct coordinates *public_Alice = entire_calculation(multiplier_Alice, &_base_Point);
 
     fprintf(stdout, "\nValue Alice received from Bob: "); FORMAL_print_point(public_Bob);
-    fprintf(stdout, "\nValue Bob received from Alice: "); FORMAL_print_point(public_Alice);
+    fprintf(stdout, "Value Bob received from Alice: "); FORMAL_print_point(public_Alice);
+    // ^^ Values parties receive from each other
 
-    struct coordinates *alice_RESULT; point_multiplication(multiplier_Alice, public_Bob, &alice_RESULT);
-    fprintf(stdout, "\nValue Alice calculates from what she received: "); FORMAL_print_point(alice_RESULT);
+    fprintf(stdout, "\nValue Alice calculates using Bob's public value: "); struct coordinates *Alice_end = entire_calculation(multiplier_Alice, public_Bob);
+    fprintf(stdout, "Value Bob calculates using Alice's public value: "); struct coordinates *Bob_end = entire_calculation(multiplier_Bob, public_Alice);
+    // ^^ Values parties synthesize individually
 
-    struct coordinates *bob_RESULT; point_multiplication(multiplier_Alice, public_Bob, &bob_RESULT);
-    fprintf(stdout, "\nValue Bob calculates from what he received: "); FORMAL_print_point(bob_RESULT);
-
-    return 0;
+    if (Alice_end->x == Bob_end->x && Alice_end->y == Bob_end->y)
+    { fprintf(stdout, "\nValues Bob and Alice calculated individually corresponded. Elliptic Curve Diffie-Hellman proof of concept successfull.\n\nReturning '0'.\n"); return 0; }
+    else { fprintf(stdout, "\nValues Bob and Alice calculated individually did not correspond. Elliptic Curve Diffie-Hellman proof of concept not successfull.\n\nReturning '-1'.\n"); return -1; }
 }
 /* MATH NOTES:
  * 1). This is the case where p and q are inverses, so here I return the identity element
- * 2). A.k.a. "s = M(M(M(3 * M(POINT.x * POINT.x)) + a) * m_inv(M(2 * POINT.y)));"
  */
