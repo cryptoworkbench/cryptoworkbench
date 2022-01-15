db18ad0f89eefc0de41e3bfd88ed5b44be0b1b2e
cryptoworkbench <nj.vanhoof@gmail.com>
2022-01-16 00:19:05 +0100

    Made "2_N_Shamir_Secret_Sharing_Sceme_PROOF_OF_CONCEPT.c" compiler friendly again.

--- SOURCE_CODE/INSTRUMENTS/2_n_Shamir_Secret_Sharing_Sceme_PROOF_OF_CONCEPT/2_n_Shamir_Secret_Sharing_Sceme_PROOF_OF_CONCEPT.c
+++ SOURCE_CODE/INSTRUMENTS/2_n_Shamir_Secret_Sharing_Sceme_PROOF_OF_CONCEPT/2_n_Shamir_Secret_Sharing_Sceme_PROOF_OF_CONCEPT.c
@@ -23,7 +23,7 @@
 #include <stdio.h>
 #include "../../libraries/functional/string.h"
 #include "../../libraries/mathematics/maths.h"
-UL MODULUS;
+ul MOD;
 // ^^ Handle library inclusions
 
 unsigned long a, _secret_B;
@@ -33,18 +33,18 @@ struct cartesian_coordinates {
     unsigned long y_coordinate;
 };
 
-unsigned long y_calculate(unsigned long x) { return (((x * a) % MODULUS) + _secret_B) % MODULUS; }
-void secret_reduce() { if (_secret_B >= MODULUS) { _secret_B %= MODULUS; printf("The secret has been reduced by mod %lu into the congruent secret %lu.\n\n", MODULUS, _secret_B); } }
+unsigned long y_calculate(unsigned long x) { return (((x * a) % MOD) + _secret_B) % MOD; }
+void secret_reduce() { if (_secret_B >= MOD) { _secret_B %= MOD; printf("The secret has been reduced by mod %lu into the congruent secret %lu.\n\n", MOD, _secret_B); } }
 
 int main(int argc, char **argv) {
-    if (2 > argc || !STR_could_be_parsed_into_UL(argv[1], &MODULUS)) { printf("%s is not MODULUS!\n", argv[1]); exit(-1); }
-    if (3 > argc || !STR_could_be_parsed_into_UL(argv[2], &a)) { printf("%s is not a!\n", argv[2]); exit(-2); } a %= MODULUS;
+    if (2 > argc || !str_represents_ul(argv[1], &MOD)) { printf("%s is not MOD!\n", argv[1]); exit(-1); }
+    if (3 > argc || !str_represents_ul(argv[2], &a)) { printf("%s is not a!\n", argv[2]); exit(-2); } a %= MOD;
     if (3 < argc) {
-	if (STR_could_be_parsed_into_UL(argv[3], &_secret_B)) secret_reduce();
+	if (str_represents_ul(argv[3], &_secret_B)) secret_reduce();
 	else { printf("The secret '%s' has not yet been transformed into numerical form!\n", argv[3]); exit(-3); }
-    } else { printf("Now please put in your secret less than %lu: ", MODULUS); fscanf(stdin, "%lu", &_secret_B); secret_reduce(); }
+    } else { printf("Now please put in your secret less than %lu: ", MOD); fscanf(stdin, "%lu", &_secret_B); secret_reduce(); }
     fprintf(stdout, "Using coordinate format: [x, f(x)]\n\nPlot for the linear function 'f(x) \u2261 %lux + %lu':\n", a, _secret_B);
-    for (unsigned long x_coordinate = 0; x_coordinate < MODULUS; x_coordinate++) {
+    for (unsigned long x_coordinate = 0; x_coordinate < MOD; x_coordinate++) {
 	struct cartesian_coordinates point_on_graph = {x_coordinate, 0};
 	point_on_graph.y_coordinate += y_calculate(x_coordinate);
 	printf("[%lu, %lu]\n", x_coordinate, y_calculate(x_coordinate));
@@ -52,20 +52,20 @@ int main(int argc, char **argv) {
 
     unsigned long x_one, x_two;
     printf("\nDO NOT CHOOSE POINT 0 SINCE IT CONTAINS YOUR SECRET!\n");
-    printf("Specification of first point by x coordinate: "); fscanf(stdin, "%lu", &x_one); unsigned long y_one = ((((x_one * a) % MODULUS) + _secret_B % MODULUS) % MODULUS);
-    printf("Specification of second point by x coordinate: "); fscanf(stdin, "%lu", &x_two); unsigned long y_two = ((((x_two * a) % MODULUS) + _secret_B % MODULUS) % MODULUS);
+    printf("Specification of first point by x coordinate: "); fscanf(stdin, "%lu", &x_one); unsigned long y_one = ((((x_one * a) % MOD) + _secret_B % MOD) % MOD);
+    printf("Specification of second point by x coordinate: "); fscanf(stdin, "%lu", &x_two); unsigned long y_two = ((((x_two * a) % MOD) + _secret_B % MOD) % MOD);
     // ^^ Get 'random' (a.k.a. chosen) coordinates
 
-    unsigned long Y_difference = y_one + (MODULUS - y_two); Y_difference %= MODULUS;
-    unsigned long X_difference = x_one + (MODULUS - x_two); X_difference %= MODULUS;
-    while (Y_difference % X_difference != 0) Y_difference += MODULUS;
+    unsigned long Y_difference = y_one + (MOD - y_two); Y_difference %= MOD;
+    unsigned long X_difference = x_one + (MOD - x_two); X_difference %= MOD;
+    while (Y_difference % X_difference != 0) Y_difference += MOD;
     // ^^ We need to make the numerator divisible by the denominator given this specific field's conditions before we should divide in this modular arithmetic. "#MODULARARITHMETICRULES"
 
     if (Y_difference / X_difference != a) { fprintf(stderr, "This proof of concept failed!, \"Y_difference / X_difference\" is not a!\n"); exit(-4); }
     else fprintf(stdout, "a = %lu / %lu = %lu\n", Y_difference, X_difference, a);
     // ^^ Exit when this proof of concept already failed
 
-    if (_secret_B == (y_one + (MODULUS - (x_one * a) % MODULUS)) % MODULUS) fprintf(stdout, "Calculation of B succeeded: %lu\n", _secret_B);
+    if (_secret_B == (y_one + (MOD - (x_one * a) % MOD)) % MOD) fprintf(stdout, "Calculation of B succeeded: %lu\n", _secret_B);
     else { fprintf(stderr, "Calculation of B failed!, so this proof of concept failed!\n"); exit(-5); }
     return 0;
 }
