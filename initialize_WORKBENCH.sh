69b0c4d691f3bf2357c6bba8a407f42dca85ffca
cryptoworkbench <nj.vanhoof@gmail.com>
2022-01-16 16:30:41 +0100

    Edited "initialize_WORKBENCH.sh" to reflect renamings.

--- initialize_WORKBENCH.sh
+++ initialize_WORKBENCH.sh
@@ -3,26 +3,26 @@
 mkdir WORKBENCH/
 mkdir WORKBENCH/ARCHIVE
 clear && echo "########### ASSEMBLY LINE ROLLING ! #######=====>"
-cd SOURCE_CODE/INSTRUMENTS/additive_inverse && make additive_inverse && echo "#######===> The following instrument has been assemblied/linked: additive_inverse
+cd SOURCE_CODE/TOOLS/additive_inverse && make additive_inverse && echo "#######===> The following tool has been assemblied/linked: additive_inverse
 "
-cd ../modular_exponentiation && make modular_exponentiation && echo "#######===> The following instrument has been assemblied/linked: modular_exponentiation
+cd ../modular_exponentiation && make modular_exponentiation && echo "#######===> The following tool has been assemblied/linked: modular_exponentiation
 "
-cd ../finite_field_division && make finite_field_division && echo "#######===> The following instrument has been assemblied/linked: finite_field_division
+cd ../finite_field_division && make finite_field_division && echo "#######===> The following tool has been assemblied/linked: finite_field_division
 " # ^-- The tool 'finite_field_division' is a dependency of 'multiplicative_inverse' -->
-cd ../multiplicative_inverse && make multiplicative_inverse && echo "#######===> The following instrument has been assemblied/linked: multiplicative_inverse
+cd ../multiplicative_inverse && make multiplicative_inverse && echo "#######===> The following tool has been assemblied/linked: multiplicative_inverse
 "
-echo "#######===> THE FOLLOWING BASIC INSTRUMENTS HAVE BEEN ASSEMBLED/LINKED: -->                         <===#######
+echo "#######===> THE FOLLOWING BASIC TOOLS HAVE BEEN ASSEMBLED/LINKED: -->                         <===#######
 ~ additive_inverse
 ~ modular_exponentiation
 ~ finite_field_division
 ~ multiplicative_inverse
 " # ^-- Notify end of first stage of compilation.
 
-cd ../polynomial_function_over_a_GF && make polynomial_function_over_a_GF && echo "#######===> The following instrument has been assemblied/linked: polynomial_function_over_a_GF
+cd ../polynomial_function_over_a_GF && make polynomial_function_over_a_GF && echo "#######===> The following tool has been assemblied/linked: polynomial_function_over_a_GF
 "
-cd ../2_n_Shamir_Secret_Sharing && make 2_n_Shamir_Secret_Sharing && echo "#######===> The following instrument has been assemblied/linked: 2_n_Shamir_Secret_Sharing
+cd ../2_n_Shamir_Secret_Sharing && make 2_n_Shamir_Secret_Sharing && echo "#######===> The following tool has been assemblied/linked: 2_n_Shamir_Secret_Sharing
 "
-cd ../3_n_Shamir_Secret_Sharing && make 3_n_Shamir_Secret_Sharing && echo "#######===> The following instrument has been assemblied/linked: 3_n_Shamir_Secret_Sharing
+cd ../3_n_Shamir_Secret_Sharing && make 3_n_Shamir_Secret_Sharing && echo "#######===> The following tool has been assemblied/linked: 3_n_Shamir_Secret_Sharing
 "
 echo "#######===> SOME TOOLS FOR EXPLORING SHAMIR'S SECRET SHARING SCEME HAVE BEEN ASSEMBLED/LINKED: -->  <===#######
 ~ polynomial_function_over_a_GF
@@ -30,15 +30,15 @@ echo "#######===> SOME TOOLS FOR EXPLORING SHAMIR'S SECRET SHARING SCEME HAVE BE
 ~ 3_n_Shamir_Secret_Sharing
 " # ^-- Notify end of second stage of compilation.
 
-cd ../modular_group_element_table_generator && make modular_group_element_table_generator && echo "#######===> The following instrument has been assemblied/linked: modular_group_element_table_generator
+cd ../modular_group_element_table_generator && make modular_group_element_table_generator && echo "#######===> The following tool has been assemblied/linked: modular_group_element_table_generator
 "
-cd ../subgroup_examplifier && make subgroup_examplifier && echo "#######===> The following instrument has been assemblied/linked: subgroup_examplifier
+cd ../subgroup_examplifier && make subgroup_examplifier && echo "#######===> The following tool has been assemblied/linked: subgroup_examplifier
 "
-cd ../Diffie_Hellman_Key_Agreement && make Diffie_Hellman_Key_Agreement && echo "#######===> The following instrument has been assemblied/linked: Diffie_Hellman_Key_Agreement
+cd ../Diffie_Hellman_Key_Agreement && make Diffie_Hellman_Key_Agreement && echo "#######===> The following tool has been assemblied/linked: Diffie_Hellman_Key_Agreement
 "
-cd ../cayley_table_generator && make cayley_table_generator && echo "#######===> The following instrument has been assemblied/linked: cayley_table_generator
+cd ../cayley_table_generator && make cayley_table_generator && echo "#######===> The following tool has been assemblied/linked: cayley_table_generator
 "
-# cd ../prime_table_generator && make prime_table_generator && echo "#######===> The following instrument has been assemblied/linked: prime_table_generator
+# cd ../prime_table_generator && make prime_table_generator && echo "#######===> The following tool has been assemblied/linked: prime_table_generator
 #"
 echo "#######===> SOME MORE TOOLS HAVE BEEN ASSEMBLED/LINKED: -->                                         <===#######
 ~ modular_group_element_table_generator 
@@ -47,11 +47,11 @@ echo "#######===> SOME MORE TOOLS HAVE BEEN ASSEMBLED/LINKED: -->               
 ~ cayley_table_generator
 " # ^-- Notify end of third stage of compilation.
 
-cd ../Wipe_ARCHIVE && make Wipe_ARCHIVE && echo "#######===> The following instrument has been assemblied/linked: Wipe_ARCHIVE
+cd ../Wipe_ARCHIVE && make Wipe_ARCHIVE && echo "#######===> The following tool has been assemblied/linked: Wipe_ARCHIVE
 "
-cd ../View_LOGBOOK && make View_LOGBOOK && echo "#######===> The following instrument has been assemblied/linked: View_LOGBOOK
+cd ../View_LOGBOOK && make View_LOGBOOK && echo "#######===> The following tool has been assemblied/linked: View_LOGBOOK
 "
-cd ../Wipe_LOGBOOK && make Wipe_LOGBOOK && echo "#######===> The following instrument has been assemblied/linked: Wipe_LOGBOOK
+cd ../Wipe_LOGBOOK && make Wipe_LOGBOOK && echo "#######===> The following tool has been assemblied/linked: Wipe_LOGBOOK
 "
 echo "#######===> THE LABORATORY HOUSEHOLD EXECUTABLES HAVE BEEN ASSEMBLED/LINKED, NAMELY: -->             <===#######
 ~ Wipe_ARCHIVE
