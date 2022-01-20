1dd194c33d01488973c16f1c84c3256cc9139375
cryptoworkbench <nj.vanhoof@gmail.com>
2022-01-20 17:43:59 +0100

    another bunch of improvements to the end of explaining various factorization method based on human readable c code and the interactive executables taht said c code yields.

--- SOURCE_CODE/TOOLS/factorize/factorize.c
+++ SOURCE_CODE/TOOLS/factorize/factorize.c
@@ -9,6 +9,7 @@ ul MOD; // < This time we will use 'MOD' for the odd composite
 char *INEFFICIENT_trail_division = "inefficient_trail_division";
 char *MORE_EFFICIENT_trail_division = "more_efficient_trail_division";
 char *MOST_EFFICIENT_trail_division = "most_efficient_trail_division";
+char *difference_of_squares_factorization_METHOD = "difference_of_squares_factorization_method";
 char *fermats_factorization_METHOD = "fermats_factorization_method";
 
 void unrecognized_APPROACH(char *argv_two) {
@@ -16,13 +17,14 @@ void unrecognized_APPROACH(char *argv_two) {
     fprintf(stderr, "1). %s\n", INEFFICIENT_trail_division);
     fprintf(stderr, "2). %s\n", MORE_EFFICIENT_trail_division);
     fprintf(stderr, "3). %s\n", MOST_EFFICIENT_trail_division);
+    fprintf(stderr, "4). %s\n", difference_of_squares_factorization_METHOD);
     fprintf(stderr, "4). %s\n\n", fermats_factorization_METHOD);
     fprintf(stderr, "Terminating with exit status '-1'.\n");
     exit(-1);
 }
 
 int main(int argc, char **argv) {
-    if (2 > argc || !match_variadic(argv[1], 4, INEFFICIENT_trail_division, MORE_EFFICIENT_trail_division, MOST_EFFICIENT_trail_division, fermats_factorization_METHOD)) unrecognized_APPROACH(argv[1]);
+    if (2 > argc || !match_variadic(argv[1], 5, INEFFICIENT_trail_division, MORE_EFFICIENT_trail_division, MOST_EFFICIENT_trail_division, difference_of_squares_factorization_METHOD, fermats_factorization_METHOD)) unrecognized_APPROACH(argv[1]);
     if (3 > argc || !str_represents_ul(argv[2], &MOD)) { fprintf(stderr, "Failed to interpret composite '%s'!\n\nTerminating with exit status '-1'.\n", argv[2]); exit(-2); }
 
     _factorization_method factorization_method_chosen = fermat_factorization;
@@ -35,6 +37,10 @@ int main(int argc, char **argv) {
     } else if (strcmp(argv[1], MOST_EFFICIENT_trail_division) == 0) {
 	fprintf(stdout, "Using trail division and checking for all x <= %lu if x divides %lu.", DOWN_ROUNDED_second_root(MOD), MOD);
 	factorization_method_chosen = most_efficient_trail_division;
+    } else if (strcmp(argv[1], difference_of_squares_factorization_METHOD) == 0) { if (MOD % 2 == 0)
+	{ fprintf(stderr, "%lu \u2261 0	(modulus 2)\n\nThe difference of squares method can only handle odd numbers, but %lu is even. Terminating with exit status '-3'.\n", MOD, MOD); return -3; }
+	fprintf(stderr, "Using the difference of squares method.");
+	factorization_method_chosen = difference_of_squares_factorization_method;
     } else fprintf(stdout, "Using fermat's factorization method."); fprintf(stdout, "\n\n");
 
     struct ordered_pair factorization_of_MOD = factorization_method_chosen(MOD);
