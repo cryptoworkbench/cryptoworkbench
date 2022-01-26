6d020e8ce55d5a28b0bf0f41a63b3b5256ae8510
cryptoworkbench <nj.vanhoof@gmail.com>
2022-01-26 18:46:14 +0100

    more changes.

--- SOURCE_CODE/TOOLS/factorization_based_primality_test/factorization_based_primality_test.c
+++ SOURCE_CODE/TOOLS/factorization_based_primality_test/factorization_based_primality_test.c
@@ -3,7 +3,6 @@
 #include "../../libraries/functional/string.h" // needed for 'str_represents_ul()', 'BUFFER_OF_SIZE()', EXIT_STATUS_GOODBYE
 #include "../../libraries/mathematics/factorization_methods.h" // needed for 'translate_SUBTRACT_ONE()'
 #include "../../libraries/mathematics/primality_testing_scemes.h" // needed for 'SET_preferred_PRIMALITY_TEST()', 'FACTORIZATION_METHOD_UNCHOSEN()'
-unsigned long MOD;
 
 const char *chosen_PRIMALITY_TEST() { _primality_test PRIMALITY_TEST = preferred_PRIMALITY_TEST_REPORT();
     if (PRIMALITY_TEST == efficient_trial_division_PRIMALITY_TEST) return A_REPORT();
@@ -17,8 +16,8 @@ const char *chosen_PRIMALITY_TEST() { _primality_test PRIMALITY_TEST = preferred
     else return NULL;
 }
 
-int main(int argc, char **argv) { 
-    if (2 > argc || !str_represents_ul(argv[1], &MOD)) exit(-1); char *ptr = argv[2];
+int main(int argc, char **argv) { unsigned long potential_prime;
+    if (2 > argc || !str_represents_ul(argv[1], &potential_prime)) exit(-1); char *ptr = argv[2];
     if (!ptr) {
 	FILE *file;
 	if (!(file = fopen(preferred_factorization_engine_file_REPORT(), "r"))) { fprintf(stderr, "Couldn't open preferences file '%s'. "EXIT_STATUS_GOODBYE, preferred_factorization_engine_file_REPORT(), -1); exit(-1); }
@@ -28,10 +27,11 @@ int main(int argc, char **argv) {
     if (SELECTOR) SET_preferred_PRIMALITY_TEST(SELECTOR - 1);
     else FACTORIZATION_METHOD_UNCHOSEN(ptr);
 
-    fprintf(stdout, "Using the factorization engine \"%s\" as the basis for my primality test on %lu.", chosen_PRIMALITY_TEST(), MOD); if (!(argc < 3)) fprintf(stdout, "	(as specified by terminal argument)");
-    fprintf(stdout, "\n\n");
+    fprintf(stdout, "Using the factorization engine \"%s\" as the basis for my primality test on %lu.", chosen_PRIMALITY_TEST(), potential_prime);
+    if (!(argc < 3)) fprintf(stdout, "	(as specified by terminal argument)"); fprintf(stdout, "\n\n");
 
-    if (prime(MOD)) fprintf(stdout, "%lu is prime.\n", MOD);
-    else fprintf(stdout, "%lu is not prime.\n", MOD);
+    fprintf(stdout, "%lu is ", potential_prime);
+    if (prime(potential_prime, NULL)) fprintf(stdout, "prime.\n", potential_prime);
+    else fprintf(stdout, "not prime.\n", potential_prime);
     return 0;
 }
--- SOURCE_CODE/libraries/mathematics/primality_testing_scemes.c
+++ SOURCE_CODE/libraries/mathematics/primality_testing_scemes.c
@@ -22,13 +22,13 @@ int fermats_DETERMINISTIC_primality_test(unsigned long potential_prime) { for (u
 
 _primality_test primality_test(int SELECTOR) {
     switch (SELECTOR) {
-	case 7: return efficient_trial_division_PRIMALITY_TEST; case 6: return LESS_efficient_trial_division_PRIMALITY_TEST; case 5: return LEAST_efficient_trial_division_PRIMALITY_TEST;
-	case 4: return efficient_trial_division_TABLE_AIDED_PRIMALITY_TEST; case 3: return LESS_efficient_trial_division_TABLE_AIDED_PRIMALITY_TEST;
-	case 2: return LEAST_efficient_trial_division_TABLE_AIDED_PRIMALITY_TEST; case 1: return shor_factorization_PRIMALITY_TEST;
-	case 0: return fermat_factorization_PRIMALITY_TEST; default: return NULL; };
+	case 7: return fermat_factorization_PRIMALITY_TEST; case 6: return shor_factorization_PRIMALITY_TEST; case 5: return LEAST_efficient_trial_division_TABLE_AIDED_PRIMALITY_TEST;
+	case 4: return LESS_efficient_trial_division_TABLE_AIDED_PRIMALITY_TEST; case 3: return efficient_trial_division_TABLE_AIDED_PRIMALITY_TEST; case 2: return LEAST_efficient_trial_division_PRIMALITY_TEST;
+	case 1: return LESS_efficient_trial_division_PRIMALITY_TEST; case 0: return efficient_trial_division_PRIMALITY_TEST; default: return NULL; };
 } // ^ A similar function for returning pointers to primality test functions in 'primality_testing_scemes.c' as there was a function for returning pointers to factorization functions in 'factorization.h':
 /// Only difference now is that there are 9 primality tests, while there were 8 factorization algorithms; this discrapency is not accounted for
 
 int primality_test_based_on_preferred_factorization_engine(unsigned long potential_prime) { return (potential_prime - preferred_factorization_ENGINE(potential_prime)) ? ADDITIVE_IDENTITY : MULTIPLICATIVE_IDENTITY; }
 void SET_preferred_PRIMALITY_TEST(int SELECTOR) { preferred_PRIMALITY_TEST = primality_test(SELECTOR); }
-int prime(unsigned long potential_prime) { return preferred_PRIMALITY_TEST(potential_prime); }
+int prime(unsigned long potential_prime, _primality_test alternative_primality_test_specification)
+{ if (!alternative_primality_test_specification) alternative_primality_test_specification = preferred_PRIMALITY_TEST; return alternative_primality_test_specification(potential_prime); }
