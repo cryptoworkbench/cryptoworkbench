202481aa235879eee71b2440d8fe4e06441a4595
cryptoworkbench <nj.vanhoof@gmail.com>
2022-01-30 19:43:34 +0100

    nice.

--- SOURCE_CODE/TOOLS/polynomial_over_GF/polynomial_over_GF.c
+++ SOURCE_CODE/TOOLS/polynomial_over_GF/polynomial_over_GF.c
@@ -9,21 +9,20 @@ int main(int argc, char **argv) {
     if (2 > argc || !str_represents_ul(argv[1], _REPORT_LOCATION_OF_mod())) { fprintf(stderr, "%s is not mod!\n", argv[1]); exit(-1); } ul x;
     // ^^ Process mandatory variables
 
-    ul number_of_coefficients = argc - 2;
-    if (number_of_coefficients) {
-	unsigned long **coefficients = (unsigned long **) malloc(sizeof(unsigned long *) * (number_of_coefficients + 1));
-	for (ul i = 0; i < number_of_coefficients; i++) coefficients[i] = (unsigned long *) malloc(sizeof(unsigned long)); coefficients[number_of_coefficients] = NULL;
-	// ^ Prepare array of appriopiate size
+    unsigned long number_of_coefficients;
+    if (!(number_of_coefficients = argc - 2)) { fprintf(stderr, "Please fill out the remainder of the arguments with coefficients.\n\nTerminating with exit status '-4'.\n"); return -4; }
+    unsigned long **coefficients = (unsigned long **) malloc(sizeof(unsigned long *) * (number_of_coefficients + 1));
+    for (ul i = 0; i < number_of_coefficients; i++) coefficients[i] = (unsigned long *) malloc(sizeof(unsigned long)); coefficients[number_of_coefficients] = NULL;
+    // ^ Prepare array of appriopiate size
 
-	// for (ul i = 0; i + 2 < argc; i++) if (!str_represents_ul(argv[i + 2], coefficients[i])) { fprintf(stderr, "%s is not interpretable.\n", argv[i + 2]); return -2; }
-	for (ul i = 0; i < number_of_coefficients; i++) if (!str_represents_ul(argv[i + 2], coefficients[i])) { fprintf(stderr, "%s is not interpretable.\n", argv[i + 2]); return -2; }
+    // for (ul i = 0; i + 2 < argc; i++) if (!str_represents_ul(argv[i + 2], coefficients[i])) { fprintf(stderr, "%s is not interpretable.\n", argv[i + 2]); return -2; }
+    for (ul i = 0; i < number_of_coefficients; i++) if (!str_represents_ul(argv[i + 2], coefficients[i])) { fprintf(stderr, "%s is not interpretable.\n", argv[i + 2]); return -2; }
 
-	fprintf(stdout, "(x, y) : 'y \u2261 ", _REPORT_mod());
-	ul i = 0; while (1) {
-	    fprintf(stdout, "%lu", *coefficients[i]);
-	    if (!coefficients[i + 1]) break; fprintf(stdout, " * x^%lu + ", i); i++;
-	} fprintf(stdout, "	(mod %lu)'\n", _REPORT_mod()); // \u21E8 \u2794 \u279C \u279F \u27A0
-	
-	for (ul x = 0; x < _REPORT_mod(); x++) fprintf(stdout, "(%lu, %lu)\n", x, polynomial_over_GF(coefficients, x)); return 0;
-    } else { fprintf(stderr, "Please fill out the remainder of the arguments with coefficients.\n\nTerminating with exit status '-4'.\n"); return -4; }
+    fprintf(stdout, "(x, y) : 'y \u2261 ", _REPORT_mod());
+    ul i = 0; while (1) {
+	fprintf(stdout, "%lu", *coefficients[i]);
+	if (!coefficients[i + 1]) break; fprintf(stdout, " * x^%lu + ", i); i++;
+    } fprintf(stdout, "	(mod %lu)'\n", _REPORT_mod()); // \u21E8 \u2794 \u279C \u279F \u27A0
+    
+    for (ul x = 0; x < _REPORT_mod(); x++) fprintf(stdout, "(%lu, %lu)\n", x, polynomial_over_GF(coefficients, x)); return 0;
 }
