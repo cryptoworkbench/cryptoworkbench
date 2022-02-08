1b46119170ee19144cd3206b68945326288ae73c
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-08 21:42:04 +0100

    mostly irrelevant update for "subgroup_examplifier.c".

--- SOURCE_CODE/TOOLS/subgroup_examplifier/subgroup_examplifier.c
+++ SOURCE_CODE/TOOLS/subgroup_examplifier/subgroup_examplifier.c
@@ -87,12 +87,13 @@ unsigned long second_MAIN(struct VOID_ptr_ptr_PAIR element_CHANNEL_PTR_pair) { u
 int main(int argc, char **argv) { mod_ = (unsigned long *) malloc(sizeof(unsigned long)); unparsed_arg_ = argv[1];
     if (!str_represents_ul(unparsed_arg_, mod_)) error_message(error_selector(1), -1); int *SELECTOR = (int *) malloc(sizeof(int)); unparsed_arg_ = argv[2];
     if (10 == (*SELECTOR = identity_SELECTOR(unparsed_arg_))) error_message(error_selector(2), -2); id_ = (unsigned long *) malloc(sizeof(unsigned long)); *id_ = identity_(*SELECTOR); free(SELECTOR);
+    if (!(*mod_) || !(*mod_ - 1) && *id_) error_message(error_selector(3), -3);
     offset = (struct ordered_pair *) malloc(sizeof(struct ordered_pair)); offset->a = offset->b = 0; // member a will hold y offset, member b will hold x offset
     if (argc != 3) { switch (argc) {
 	    case 5: if (!str_represents_ul(argv[4], &offset->a)) fprintf(stderr, "Failed to interpret vertical table offset. Defaulting to not using any.\n");
 	    case 4: if (!str_represents_ul(argv[3], &offset->b)) fprintf(stderr, "Failed to interpret horizontal table offset. Defaulting to not using any.\n");
 	    default: if (!(*id_)) { offset->a %= *mod_; offset->b %= *mod_; } };
-    } if (!(*mod_) || !(*mod_ - 1) && *id_) { free(offset); error_message(error_selector(3), -3); }
+    }
     // process terminal arguments ^
 
     unsigned long generator_count = second_MAIN(group_elements_LL(argv));
