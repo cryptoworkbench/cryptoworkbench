8123c7d1074014acedac3454e9dd91b6c609f4fc
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-16 20:46:21 +0100

    ambition description within TOOL "legendre_symbol".

--- SOURCE_CODE/TOOLS/legendre_symbol/legendre_symbol.c
+++ SOURCE_CODE/TOOLS/legendre_symbol/legendre_symbol.c
@@ -1,27 +1,31 @@
-// Here I will calculate the Legende symbol using Euler's criterion.:q
+// legendre_symbol() seems to calculate Legendre's symbol using Euler's criterion. Good.
+// I need to make a wrapper function which factorizes 'p' in order to be able to use legendre_symbol() as a backhand
+// and calculate any:
+// ( __composite__ / P )
+//
+// LEGENDRE SYMBOL.
 #include <stdio.h>
 #include "../../libraries/functional/string.h"
 #include "../../libraries/mathematics/maths.h"
 #include "../../libraries/mathematics/factorization_methods.h"
 #include "../../libraries/mathematics/primality_testing.h"
 
-int main(int argc, char **argv) { const char *specific_file = _REPORT_preferred_factorization_engine_file();
-    FILE *file; if (!(file = fopen(specific_file, "r"))) { fprintf(stderr, "Couldn't open preferences file '%s'." EXIT_STATUS_GOODBYE, specific_file, -1); exit(-1); }
-    // ^ open file required for reading
-
-    char *BUFFER = BUFFER_OF_SIZE(200); fscanf(file, "%s[^\n]", BUFFER); fclose(file);
-    // ^ copy first 200 bytes from file into BUFFER
-
-    int SELECTOR = SELECTOR_from_str_representing_factorization_method(BUFFER); if (SELECTOR) preferred_factorization_ENGINE = factorization_method(SELECTOR - 1); else FACTORIZATION_METHOD_UNCHOSEN(BUFFER); free(BUFFER);
-    // ^ Now engine should be set
-
+int main(int argc, char **argv) {
     unsigned long p; if (2 > argc || !str_represents_ul(argv[1], &p)) { fprintf(stderr, "Failed to interpret %s as 'p'.\n", argv[1]); exit(-2); }
     unsigned long q; if (3 > argc || !str_represents_ul(argv[2], &q)) { fprintf(stderr, "Failed to interpret %s as 'q'.\n", argv[2]); exit(-3); }
     // ^ interpret required primes
 
-    if (!primality_test_based_on_preferred_factorization_engine(p)) { fprintf(stderr, "p (%lu) is not prime.\n", p); exit(-4); }
-    if (!primality_test_based_on_preferred_factorization_engine(q)) { fprintf(stderr, "q (%lu) is not prime.\n", q); exit(-5); }
-    if (p > q) { p %= q; fprintf(stdout, "Reduced p modulus q.\n"); }
-    fprintf(stdout, "p = %lu\n", p); fprintf(stdout, "q = %lu\n\n", q); fprintf(stdout, "The Legendre symbol of p over q ( p / q ) = %lu over %lu = ( %lu / %lu ) = %i\n", p, q, p, q, legendre_symbol(p, q));
+    char *ptr = argv[3]; if (!ptr) ptr = query_preferences_file();
+    if (!(_preferred_primality_test = primality_test(SELECTOR_from_str_representing_factorization_method(ptr)))) {
+	fprintf(stderr, "Failed to interpret '%s' from ", ptr); if (argv[3]) fprintf(stderr, "terminal argument");
+	else fprintf(stderr, "global preferences file '%s'", _REPORT_preferred_factorization_engine_file()); fprintf(stderr, ".\n\n");
+	char *UPDATE_VALUE; _preferred_primality_test = primality_test(SELECTOR_from_str_representing_factorization_method(UPDATE_VALUE = STDIN_factorization_engine(ptr)));
+	if (!argv[3]) { write_to_preferences_file(UPDATE_VALUE, fopen(_REPORT_preferred_factorization_engine_file(), "w")); fprintf(stdout, "Updated preferences file.\n\n"); }
+    } // if both the terminal argument and the preferences file were unintelligeble, then force take factorization method from STDIN
+
+    if (!prime(p, NULL)) { fprintf(stderr, "p (%lu) is not prime.\n", p); exit(-4); }
+    if (!prime(q, NULL)) { fprintf(stderr, "q (%lu) is not prime.\n", q); exit(-5); }
+    if (p > q) { p %= q; fprintf(stdout, "Reduced p modulus q.\n"); } // fprintf(stdout, "p = %lu\n", p); fprintf(stdout, "q = %lu\n\n", q);
+    fprintf(stdout, "The Legendre symbol of p over q ( p / q ) = %lu over %lu = ( %lu / %lu ) = %i\n", p, q, p, q, legendre_symbol(p, q));
     return 0;
 }
