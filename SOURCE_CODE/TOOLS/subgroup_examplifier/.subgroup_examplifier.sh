785a0936c369c8c62ab36244b935160603222830
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-18 22:37:55 +0100

    hm.

--- SOURCE_CODE/TOOLS/subgroup_examplifier/.subgroup_examplifier.sh
+++ SOURCE_CODE/TOOLS/subgroup_examplifier/.subgroup_examplifier.sh
@@ -1,12 +1,26 @@
 # MAKES SUBGROUP_EXAMPLIFIER INTERACTIVE!
-# Currently shift the amount of the modulus.
-# Should cycle through a couple times.
+#
+# SUPPLY:
+# ~ as first argument the modulus
+# ~ as second argument the group's identity
+# ~ as third argument the refresh rate
+#
+# A fourth argument can be supplied in order to set the vertical offset throughout loop-time.
+# 
+# DEVELOPER NOTICE:
+# Currently shifts in total the amount of the modulus.
+# I'd prefer it if it cycled through at least three times.
 
 #!/bin/bash
 
 for i in $(seq 1 $1); do
     clear
-    echo "Current horizontal shift: $i"; echo ""
-    ./subgroup_examplifier $1 $2 "$i" 0;
-    sleep 1
+    echo "Current horizontal offset: $i
+    ";
+    ./subgroup_examplifier $1 $2 "$i" $4;
+    echo "
+    ";
+    echo "refresh rate: every $3 s.
+    ";
+    sleep $3
 done
