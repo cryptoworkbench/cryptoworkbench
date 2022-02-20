1641883a1871ab5145d90f7ee81f7bd035a0f725
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-20 20:38:50 +0100

    fixed multiplicative_inverse.c

--- SOURCE_CODE/TOOLS/multiplicative_inverse/multiplicative_inverse.c
+++ SOURCE_CODE/TOOLS/multiplicative_inverse/multiplicative_inverse.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-#include <unistd.h> // < 'execvp()'
-#include <sys/wait.h> // 'waitpid()'
-#include "../../libraries/functional/string.h" // 'str_represents_ul()'
-#include "../../libraries/mathematics/maths.h" // 'GCD()'
+#include "../../libraries/functional/string.h"
+#include "../../libraries/mathematics/maths.h"
+// library inclusions ^^^
 
-const char *argv_ONE[] = {"first", "modulus"};
-const char *argv_TWO[] = {"second", "number coprime with the modulus whose multiplicative mod_inverse you want to know"};
+void multiplicative_group_element_error() { fprintf(stderr, "Please provide as second argument an element within \u2115/\u2115%s*.", (*argv_location)[1]); }
+void mod_error() { fprintf(stderr, "Please provide as first argument a modulus so I know what multiplicative group to take the inverse within."); }
+// error functions ^^
 
-void argv_ERROR(char **argv, int index) {
-    const char **error_specific_message = argv_ONE;
-    if (index == 2) error_specific_message = argv_TWO;
-    fprintf(stderr, "%s is not something I am able to understand as a number; please provide as %s argument the %s.\n\n" EXIT_STATUS_GOODBYE, argv[index], error_specific_message[0], error_specific_message[1], -index);
-    exit(-index);
-}
-
-int main(int argc, char **argv) { unsigned long mod; mod_ = &mod;
-    if (2 > argc || !str_represents_ul(argv[1], &mod)) argv_ERROR(argv, 1); unsigned long number_coprime_to_MOD;
-    if (3 > argc || !str_represents_ul(argv[2], &number_coprime_to_MOD)) argv_ERROR(argv, 2); unsigned long GCD_of_arguments = GCD(number_coprime_to_MOD, mod);
-    if (GCD_of_arguments != 1) { fprintf(stderr, "%lu shares a factor of %lu with %lu.\n\n" EXIT_STATUS_GOODBYE, number_coprime_to_MOD, GCD_of_arguments, mod, -3); exit(-3); }
-    fprintf(stdout, "%lu^-1 \u2261 %lu	(mod %lu)\n", number_coprime_to_MOD, mod_divide(1, number_coprime_to_MOD), mod); return 0;
+int main(int argc, char **argv) { argv_location = &argv; 
+    unsigned long multiplicative_group_modulus;      conditional_goodbye(n(n(error_specification(mod_error, n(str_represents_ul_(argv[1], &multiplicative_group_modulus, -1)))))); mod_ = &multiplicative_group_modulus;
+    unsigned long element_from_multiplicative_group; conditional_goodbye(n(n(error_specification(multiplicative_group_element_error, n(str_represents_ul_(argv[2], &element_from_multiplicative_group, -2))))));
+    unsigned long GCD_of_arguments = GCD(element_from_multiplicative_group, multiplicative_group_modulus); if (GCD_of_arguments != 1)
+    { fprintf(stderr, "%lu shares a factor of %lu with %lu.\n\n" EXIT_STATUS_GOODBYE, element_from_multiplicative_group, GCD_of_arguments, multiplicative_group_modulus, -3); exit(-3); }
+    fprintf(stdout, "%lu^-1 \u2261 %lu	(mod %lu)\n", element_from_multiplicative_group, mod_divide(1, element_from_multiplicative_group), multiplicative_group_modulus); return 0;
 }
