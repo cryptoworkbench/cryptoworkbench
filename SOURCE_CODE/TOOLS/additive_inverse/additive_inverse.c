48ee1aa03c2c8ca68d1fe9800d703db72b8e0243
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-20 18:01:17 +0100

    updated additive_inverse.c

--- SOURCE_CODE/TOOLS/additive_inverse/additive_inverse.c
+++ SOURCE_CODE/TOOLS/additive_inverse/additive_inverse.c
@@ -1,20 +1,17 @@
 // Takes the additive mod_inverse of a number using the mathematics library 'maths.c' ||
-// "##### ===> END OF PROGRAM DESCRIPTION!                             <=== ####" ||
+// "##### ===> END OF PROGRAM DESCRIPTION!                                 <=== ####" ||
 
 #include <stdio.h>
-#include "../../libraries/functional/string.h" // needed for 'str_represents_ul()'
-#include "../../libraries/mathematics/maths.h" // needed for 'mod_', 'mod_inverse()'
+#include "../../libraries/functional/string.h"
+#include "../../libraries/mathematics/maths.h"
 
-const char *GENERIC_parsing_error = "%s is not something I am able to understand as a number:\n";
-const char *MOD_parsing_error = "Please provide as first argument the modulus.";
-const char *ID_parsing_error = "Please provide as second argument the number to take the additive inverse of.";
+void generic_parsing_error() { fprintf(stderr, "%s is not something I am able to understand as a number."); }
+void mod_parsing_error() { fprintf(stderr, "Please provide as first argument the modulus."); }
+void id_parsing_error() { fprintf(stderr, "Please provide as second argument the number to take the additive inverse of."); }
 
-const char *local_switch(int index) { return (index - 1) ? ID_parsing_error : MOD_parsing_error; }
-
-unsigned long mod; int main(int argc, char **argv) { mod_ = &mod; local_error_selector = local_switch;
-    if (!str_represents_ul(argv[1], mod_)) str_does_not_represent_suitable_ul(argv, 1); unsigned long inverse_of_additve_inverse;
-    if (!str_represents_ul(argv[2], &inverse_of_additve_inverse)) str_does_not_represent_suitable_ul(argv, 2);
-    unsigned long additive_inverse = mod_inverse(inverse_of_additve_inverse);
+int main(int argc, char **argv) { unsigned long mod; mod_ = &mod;
+    conditional_goodbye(n(n(error_specification(mod_parsing_error, n(str_represents_ul_(argv[1], mod_, -1)))))); unsigned long inverse_of_additve_inverse;
+    conditional_goodbye(n(n(error_specification(id_parsing_error, n(str_represents_ul_(argv[2], &inverse_of_additve_inverse, -2)))))); unsigned long additive_inverse = mod_inverse(inverse_of_additve_inverse);
     fprintf(stdout, "x - %lu \u2261 x + %lu (mod %lu)\n\n", inverse_of_additve_inverse, additive_inverse, mod);
     fprintf(stdout, "The additive inverse of %lu under modulus %lu arithmetic is %lu.\n", inverse_of_additve_inverse, mod, additive_inverse);
     return 0;
