64f9e6a2a12bc26b26dbd1cc9a3ebfe80f4b48c9
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-20 20:13:36 +0100

    fixed least_common_multiple.c

--- SOURCE_CODE/TOOLS/least_common_multiple/least_common_multiple.c
+++ SOURCE_CODE/TOOLS/least_common_multiple/least_common_multiple.c
@@ -1,15 +1,18 @@
+// Program description:
+// Takes the LCM of two numbers.
+//
+// DEVELOPERS NOTICE:
+// I wish this TOOL was like the TOOL 'greatest_common_divisor' which can take any amouny of arguments greater than one.
 #include <stdio.h>
 #include "../../libraries/mathematics/maths.h"
 #include "../../libraries/functional/string.h"
 
-const char *variable_a_ERROR = "please provide as first argument the first number to take the LCM with.";
-const char *variable_b_ERROR = "please provide as second argument the second number to take the LCM with.";
+void variable_a_error() { fprintf(stderr, "Please provide as first argument the first number to take the LCM with."); }
+void variable_b_error() { fprintf(stderr, "Please provide as second argument the second number to take the LCM with."); }
 
-const char *local_switch(int INDEX) { switch (INDEX) { case 1: return variable_a_ERROR; case 2: return variable_b_ERROR; }; }
-
-int main(int argc, char **argv) { local_error_selector = local_switch; unsigned long a;
-    if (!str_represents_ul(argv[1], &a)) str_does_not_represent_suitable_ul(argv, 1); unsigned long b;
-    if (!str_represents_ul(argv[2], &b)) str_does_not_represent_suitable_ul(argv, 2);
+int main(int argc, char **argv) {
+    unsigned long a; conditional_goodbye(n(n(error_specification(variable_a_error, n(str_represents_ul_(argv[1], &a, -1))))));
+    unsigned long b; conditional_goodbye(n(n(error_specification(variable_b_error, n(str_represents_ul_(argv[2], &b, -2))))));
     fprintf(stdout, "The least common multiple of %lu and %lu is %lu.\n", a, b, least_common_multiple(a, b));
     return 0;
 }
