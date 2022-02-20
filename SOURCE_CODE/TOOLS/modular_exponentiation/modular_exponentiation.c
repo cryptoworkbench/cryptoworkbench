3205a22304447290e7bc2fdf837f12b7d86ab569
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-20 20:21:41 +0100

    fixed modular_exponentiation.c

--- SOURCE_CODE/TOOLS/modular_exponentiation/modular_exponentiation.c
+++ SOURCE_CODE/TOOLS/modular_exponentiation/modular_exponentiation.c
@@ -1,19 +1,18 @@
-#include <stdio.h> // 'printf()'
-#include <stdlib.h> // 'malloc()'
-#include "../../libraries/functional/string.h" // 'str_represents_ul()'
-#include "../../libraries/mathematics/maths.h" // 'N_operation()'
+// Program description: performs modular exponentiation using the 'square and multiply' method for modular exponentiation (which is implemented in math.c). Not much to be said here.
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../libraries/functional/string.h"
+#include "../../libraries/mathematics/maths.h"
+// library inclusions ^
 
 void exponent_error() { fprintf(stderr, "\nFailed to understand '%s' as the value to use for the exponent.", unparsed_arg); }
 void base_error() { fprintf(stderr, "\nFailed to understand '%s' as the value to use as the base.", unparsed_arg); }
 void mod_error() { fprintf(stderr, "\nFailed to understand '%s' as the value to use as the modulus.", unparsed_arg); }
+// error functions ^^^
 
-int main(int argc, char **argv) { unsigned long mod; mod_ = &mod; unparsed_arg = argv[1];
-    if (!str_represents_ul(unparsed_arg, &mod, 0)) conditional_goodbye(n(error_specification(mod_error, -1)));
-    unsigned long base;     unparsed_arg = argv[2]; if (!str_represents_ul(unparsed_arg, &base, 0)) conditional_goodbye(n(error_specification(base_error, -2)));
-    unsigned long exponent; unparsed_arg = argv[3]; if (!str_represents_ul(unparsed_arg, &exponent, 0)) conditional_goodbye(n(error_specification(exponent_error, -3)));
-    // ^^^ Parse terminal arguments
-
-    fprintf(stdout, "%lu^%lu %% %lu = %lu\n", base, exponent, mod, mod_exponentiate(base, exponent));
-    // ^^^ Perform modular exponentiation using the "FINITE_N_exponentiation()" function
-    return 0;
+int main(int argc, char **argv) { unsigned long mod;
+    mod_ = &mod;            conditional_goodbye(n(n(error_specification(mod_error, str_represents_ul_(argv[1], mod_, -1)))));
+    unsigned long base;     conditional_goodbye(n(n(error_specification(base_error, str_represents_ul_(argv[2], &base, -1)))));
+    unsigned long exponent; conditional_goodbye(n(n(error_specification(exponent_error, str_represents_ul_(argv[3], &exponent, -1)))));
+    fprintf(stdout, "%lu^%lu %% %lu = %lu\n", base, exponent, mod, mod_exponentiate(base, exponent)); return 0;
 }
