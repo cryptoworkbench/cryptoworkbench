972be317e9715270ddf63a357739cb9480955088
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-21 14:28:45 +0100

    plot_polynomial

--- SOURCE_CODE/TOOLS/plot_polynomial/plot_polynomial.c
+++ SOURCE_CODE/TOOLS/plot_polynomial/plot_polynomial.c
@@ -4,18 +4,25 @@
 // If such argument is missing fprintf() and fscanf() can take care of that.
 #include <stdio.h>
 #include "../../libraries/functional/string.h"
-#include "../../libraries/mathematics/maths.h" // 'SINGULAR_polynomial_over_GF()'
+#include "../../libraries/mathematics/maths.h"
 
-unsigned long mod; int main(int argc, char **argv) { mod_ = &mod;
-    if (2 > argc || !str_represents_ul(argv[1], &mod)) { fprintf(stderr, "%s is not mod!\n", argv[1]); exit(-1); }
-    // ^^ Process mandatory variables
+void coefficient_failed_to_parse() {
+    fprintf(stderr, "Coefficient failed to parse!");
+}
 
-    unsigned long number_of_coefficients;
-    if (!(number_of_coefficients = argc - 2)) { fprintf(stderr, "Please fill out the remainder of the arguments with coefficients.\n\nTerminating with exit status '-4'.\n"); return -4; }
+void wrong_argument_count() { fprintf(stderr, "The no coefficients were supplied!"); }
+void mod_error() { fprintf(stderr, "'%s' is not mod!"); }
+
+int main(int argc, char **argv) {
+    unsigned long mod; conditional_goodbye(n(n(error_specification(mod_error, n(str_represents_ul_(argv[1], &mod, -1)))))); mod_ = &mod;
+    // take in mod ^
+
+    unsigned long number_of_coefficients; conditional_goodbye(n(n(error_message(wrong_argument_count, -4 * (!(number_of_coefficients = argc - 2))))));
+    // set 'number_of_coefficients' and simultaneously check if the required amount was met ^
 
     unsigned long **coefficient = (unsigned long **) malloc(sizeof(unsigned long *) * (number_of_coefficients + 1)); unsigned long i;
     for (i = 0; i < number_of_coefficients; i++) coefficient[i] = (unsigned long *) malloc(sizeof(unsigned long)); coefficient[number_of_coefficients] = NULL;
-    for (i = 0; i < number_of_coefficients; i++) if (!str_represents_ul(argv[i + 2], coefficient[i])) { fprintf(stderr, "%s is not interpretable.\n", argv[i + 2]); return -2; }
+    for (i = 0; i < number_of_coefficients; i++) conditional_goodbye(n(n(error_specification(coefficient_failed_to_parse, n(str_represents_ul_(argv[i + 2], coefficient[i], -3))))));
     
     fprintf(stdout, "Plotting polynomial with coefficients:\n");
     i = 0; char symbol = 'a'; while (1) { fprintf(stdout, "%c = %lu", symbol, *coefficient[i]); if (!coefficient[i + 1]) break; i++; symbol++; fprintf(stdout, ", "); }
@@ -26,6 +33,6 @@ unsigned long mod; int main(int argc, char **argv) { mod_ = &mod;
     if (!(*mod_)) fscanf(stdin, "%lu", &range_bound);
     else fprintf(stdout, "%lu\n", range_bound);
 
-    for (unsigned long x = 0; x < range_bound; x++) fprintf(stdout, "(%lu, %lu)\n", x, _polynomial(coefficient, x, *mod_));
+    for (unsigned long x = 0; x < range_bound; x++) fprintf(stdout, "(%lu, %lu)\n", x, mod_polynomial(coefficient, x));
     free(coefficient); return 0; }
 
