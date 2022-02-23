4534e174bf27ad3177c8986b66bd90aa58cf8d34
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-23 17:09:58 +0100

    fixed random_numbers.c

--- SOURCE_CODE/TOOLS/random_numbers/random_numbers.c
+++ SOURCE_CODE/TOOLS/random_numbers/random_numbers.c
@@ -7,14 +7,15 @@
 char *unparsed_arg;
 // global variable declarations ^
 
-void quantity_error() { fprintf(stderr, "\nFailed to interpet '%s' as amount of random numbers to generate.\n\n", unparsed_arg); }
-void mod_error() { fprintf(stderr, "\nFailed to interpet '%s' as size.\n\n", unparsed_arg); }
+void quantity_error() { fprintf(stderr, "Failed to interpet '%s' as amount of random numbers to generate.", unparsed_arg); }
+void mod_error() { fprintf(stderr, "Please provide as first argument the maximum size to the random numbers.", unparsed_arg); }
 _error_selector error_selector(int SELECTOR) { switch (SELECTOR) { case 1: return mod_error; case 2: return quantity_error; }; }
 // error functions ^
 
-int main(int argc, char **argv) { unsigned long upper_bound; unparsed_arg = argv[1];
-    if (!str_represents_ul(unparsed_arg, &upper_bound)) error_message(error_selector(1), -1); unsigned long quantity; unparsed_arg = argv[2];
-    if (!str_represents_ul(unparsed_arg, &quantity)) error_message(error_selector(2), -2);
+int main(int argc, char **argv) {
+    unparsed_arg = argv[1]; unsigned long upper_bound; conditional_goodbye(n(n(error_specification(mod_error, n(str_represents_ul_(unparsed_arg, &upper_bound, -1))))));
+    unparsed_arg = argv[2]; unsigned long quantity;    conditional_goodbye(n(n(error_specification(quantity_error, n(str_represents_ul_(unparsed_arg, &quantity, -2))))));
+    // if (!str_represents_ul(unparsed_arg, &quantity)) error_message(error_selector(2), -2);
     // interpret terminal arguments ^
 
     unsigned long i = 1; do { fprintf(stdout, "Random number %lu: %lu\n", i, urandom_number(upper_bound)); i++; } while (i != quantity + 1); close_urandom(); return 0; }
