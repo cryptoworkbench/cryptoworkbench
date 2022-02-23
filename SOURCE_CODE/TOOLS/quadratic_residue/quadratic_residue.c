67eb81323e32ab350b2e7f5585ace205e41a49cc
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-23 17:01:39 +0100

    fixed quadratic_residue.c

--- SOURCE_CODE/TOOLS/quadratic_residue/quadratic_residue.c
+++ SOURCE_CODE/TOOLS/quadratic_residue/quadratic_residue.c
@@ -1,9 +1,10 @@
 /* Program description:
- * Prints a list of quadratic residues.
+ * Print the complete list of quadratic residues.
  *
- * Should do.
+ * Can be verified using 'subgroup_examplifier':
+ * Use a horizontal offset of 2 and the first column to the right of the leftmost column* will contain only squares.
  *
- * I think it works.
+ * * the column where the generated subgroup begins.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,26 +12,22 @@
 #include "../../libraries/mathematics/maths.h"
 #include "../../libraries/functional/triple_ref_pointers.h"
 
-struct LL_ { struct LL_ *next; unsigned long e; }; // << THIS I CAN ABSTRACT OUT. It is already in "factorization.h".
-struct LL_ *quadratic_residue_LL = NULL; char *unparsed_arg;
+struct LL_ { struct LL_ *next; unsigned long e; };
+struct LL_ *quadratic_residue_LL = NULL;
 // global variable definitions ^^
+
+void mod_failed_to_parse() { fprintf(stderr, "Please provide as first argument the modulus of the multiplicative group.\n\nA.k.a. specify '\u2115/\u2115?*'"); }
 
 void least_to_most_INSERT(struct LL_ **tracer, unsigned long quadratic_residue) {
     while (*tracer && (**tracer).e <= quadratic_residue) { if ((**tracer).e == quadratic_residue) return; tracer = &(*tracer)->next; }
-    struct LL_ *new_quadratic_residue = (struct LL_ *) malloc(sizeof(struct LL_)); new_quadratic_residue->e = quadratic_residue; // new_quadratic_residue->next = NULL;
+    struct LL_ *new_quadratic_residue = (struct LL_ *) malloc(sizeof(struct LL_)); new_quadratic_residue->e = quadratic_residue;
     new_quadratic_residue->next = *tracer; *tracer = new_quadratic_residue;
 }
 
-int main(int argc, char **argv) { unsigned long mod; mod_ = &mod; unparsed_arg = argv[1];
-    if (!str_represents_ul(unparsed_arg, mod_)) { fprintf(stderr, "Mod incorrect!\n"); exit(-1); } // <-- Make an error function specifically for this case!
-    for (unsigned long i = 1; i <= mod / 2; i++) if (GCD(i, mod) == 1) least_to_most_INSERT(&quadratic_residue_LL, mod_multiply(i, i)); // # (1.
-    printf("Quadratic residue in \u2115/%lu\u2115:\n", mod);
+int main(int argc, char **argv) {
+    unsigned long mod; conditional_goodbye(n(n(error_specification(mod_failed_to_parse, n(str_represents_ul_(argv[1], &mod, -1)))))); mod_ = &mod;
+    for (unsigned long i = 1; i <= mod / 2; i++) if (GCD(i, mod) == 1) least_to_most_INSERT(&quadratic_residue_LL, mod_multiply(i, i));
+    fprintf(stdout, "Quadratic residue in \u2115/%lu\u2115:\n", mod);
     for (; quadratic_residue_LL; quadratic_residue_LL = quadratic_residue_LL->next) fprintf(stdout, "%lu\n", quadratic_residue_LL->e);
     return 0;
 }
-/* # (1. Idea for plotting elliptic curves over finite fields:
- * #	 Use Legendres symbol, Jacobis symbol, the law of quadratic reciprocity, etc. to determine if some element from a multiplicative group is a quadratic residue
- * #	 Then if you were to change the cause here from 'i <= mod / 2' to 'i < mod'
- * #     Then also add to the linked list the roots of the quadratic residues.
- * #	 You get the idea.
- */
