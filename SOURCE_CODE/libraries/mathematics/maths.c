39abae3922506cb3b86b536aa7b9aa70b884062a
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-24 16:21:36 +0100

    removed an unneccesary function.

--- SOURCE_CODE/libraries/mathematics/maths.c
+++ SOURCE_CODE/libraries/mathematics/maths.c
@@ -76,12 +76,9 @@ unsigned long *UL_array_of_SIZE(int SIZE) {
     return ret_val;
 }
 
-int UL_array_SIZE(unsigned long **UL_array) { int ret_val = 0; while (UL_array[ret_val]) ret_val++; return ret_val; }
-
 unsigned long INDEX_within_UL_array(unsigned long *UL_array, unsigned long UL_array_SIZE, unsigned long number) { for (unsigned long INDEX = 0; INDEX < UL_array_SIZE; INDEX++) if (UL_array[INDEX] == number) return INDEX;}
 
-unsigned long _polynomial(unsigned long x, unsigned long *coefficient, unsigned long number_of_coefficients, unsigned long mod_) {
-    struct ordered_pair iso = _isomorphism();
+unsigned long _polynomial(unsigned long x, unsigned long *coefficient, unsigned long number_of_coefficients, unsigned long mod_) { struct ordered_pair iso = _isomorphism();
     do { number_of_coefficients--; iso.a = _add(iso.a, _multiply(iso.b, coefficient[number_of_coefficients], mod_), mod_); iso.b = _multiply(iso.b, x, mod_); } while (number_of_coefficients != 0); return iso.a;
 } unsigned long mod_polynomial(unsigned long x, unsigned long *coefficient, unsigned long number_of_coefficients) { return _polynomial(x, coefficient, number_of_coefficients, *mod_); }
 
