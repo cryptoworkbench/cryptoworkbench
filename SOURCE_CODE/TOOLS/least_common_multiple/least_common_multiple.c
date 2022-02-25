d010d83c968a5c1ce46ecf803cdcf468ca52a583
cryptoworkbench <nj.vanhoof@gmail.com>
2022-02-25 17:36:42 +0100

    improved least_common_multiple.c

--- SOURCE_CODE/TOOLS/least_common_multiple/least_common_multiple.c
+++ SOURCE_CODE/TOOLS/least_common_multiple/least_common_multiple.c
@@ -1,18 +1,21 @@
-// Program description:
-// Takes the LCM of two numbers.
-//
-// DEVELOPERS NOTICE:
-// I wish this TOOL was like the TOOL 'greatest_common_divisor' which can take any amouny of arguments greater than one.
+// Takes the GCD of an arbitrary number of numbers
 #include <stdio.h>
-#include "../../libraries/mathematics/maths.h"
 #include "../../libraries/functional/string.h"
+#include "../../libraries/mathematics/maths.h"
+// library inclusions ^
 
-void variable_a_error() { fprintf(stderr, "Please provide as first argument the first number to take the LCM with."); }
-void variable_b_error() { fprintf(stderr, "Please provide as second argument the second number to take the LCM with."); }
+unsigned long *arguments_array; int lcm_function_argument_count, i;
+// global variable declarations ^
 
-int main(int argc, char **argv) {
-    unsigned long a; conditional_goodbye(n(n(error_specification(variable_a_error, n(str_represents_ul(argv[1], &a, -1))))));
-    unsigned long b; conditional_goodbye(n(n(error_specification(variable_b_error, n(str_represents_ul(argv[2], &b, -2))))));
-    fprintf(stdout, "The least common multiple of %lu and %lu is %lu.\n", a, b, LCM(a, b));
-    return 0;
+void argument_parse() { fprintf(stderr, "Failed to parse '%s'!", (*argv_location)[1 + i]); };
+void argument_count() { fprintf(stderr, "The LCM (Least Common Multiple) function requires at least 2 arguments!"); }
+// error functions ^
+
+void ans_print() { fprintf(stdout, "%lu", arguments_array[i]); if (i + 1 == lcm_function_argument_count) return; i++; fprintf(stdout, ", "); ans_print(); }
+
+int main(int argc, char **argv) { argv_location = &argv;
+    conditional_goodbye(n(n(error_specification(argument_count, -(2 > (lcm_function_argument_count = argc - 1))))));
+    arguments_array = (unsigned long *) malloc(sizeof(unsigned long) * lcm_function_argument_count); for (i = 0; i < lcm_function_argument_count; i++)
+    conditional_goodbye(n(n(error_specification(argument_parse, n(str_represents_ul(argv[1 + i], i + arguments_array, -(2 + i))))))); i = 0;
+    fprintf(stdout, "LCM("); ans_print(); fprintf(stdout, ") = %lu\n", UNRESTRICTED_LCM(arguments_array, lcm_function_argument_count)); return 0;
 }
