a84e160dc43c8e0618368252c3fd5360da369339
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-02 17:42:41 +0100

    done for 03/02 of today.

--- reinitialize_repository.sh
+++ reinitialize_repository.sh
@@ -3,13 +3,13 @@
 clear
 rm -r applications/ 2> /dev/null;
 if [ $? -eq 0 ];
-then echo "Removed 'applications/'";
-else echo "Did not have to remove 'applications/'";
+then echo "removed 'applications/'";
+else echo "DID NOT HAVE TO REMOVE 'applications/' (good)";
 # else echo "Did not have to remove 'applications/', it seems like somoone already removed it, or you must be initializing using this reinitialization script (nothing wrong with that!).
 # "
 fi;
 
-echo "Continue in '__initialize_repository.sh' ...
+echo "continuing over at '__initialize_repository.sh'.
 "; ./__initialize_repository.sh
 
 #echo "Handing over control to '__initialize_repository.sh'		(a.k.a. continuing over at '__initialize_repository.sh')
