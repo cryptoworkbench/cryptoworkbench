248fbc5abd739df040e3c5d1768ec6c57556ad5f
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-02 15:43:05 +0100

    nice.

--- SOURCE_CODE/TOOLS/multiplicative_inverse/multiplicative_inverse.c
+++ SOURCE_CODE/TOOLS/multiplicative_inverse/multiplicative_inverse.c
@@ -16,11 +16,11 @@ void mod_error() { fprintf(stderr, "Please provide as first argument a modulus s
 void not_a_member_of_an_equivalence_class_from_the_quotient_group()
 {
     fprintf(stderr, "%lu | %lu  &&  %lu | %lu\n", common_divisor, denominator, common_divisor, *mod_);
-    fprintf(stderr, "--> %i < GCD(%lu, %lu) > %i\n", NUMERATOR, denominator, *mod_, NUMERATOR);
-    fprintf(stderr, "--> %lu is not coprime to %lu\n", denominator, *mod_);
-    fprintf(stderr, "--> %lu is neither an element from \u2115%lu*, nor from \u2115/%lu\u2115*; or representing an element from \u2115/%lu\u2115* !\n", denominator, *mod_, *mod_, *mod_);
-    fprintf(stderr, "--> %lu does not have a multiplicative inverse within \U0001D53D%lu!", denominator, *mod_);
-
+    fprintf(stderr, "## --> %i < GCD(%lu, %lu)  &&  GCD(%lu, %lu) > %i\n", NUMERATOR, denominator, *mod_, denominator, *mod_, NUMERATOR);
+    fprintf(stderr, "## --> %lu is not coprime to %lu\n", denominator, *mod_);
+    fprintf(stderr, "## --> %lu is neither an element from \u2115%lu*, nor from \u2115/%lu\u2115*; or representing an element from \u2115/%lu\u2115* !\n", denominator, *mod_, *mod_, *mod_);
+    fprintf(stderr, "## --> %lu does not have a multiplicative inverse within \U0001D53D%lu!\n", denominator, *mod_);
+    fprintf(stderr, "#");
     // fprintf(stderr, " %lu | %lu  &&  %lu | %lu\n-->  1 < GCD(%lu, %lu)\n-->  %lu is not coprime with %lu\n-->  %lu is neither from \u2115%lu*; nor from \u2115/%lu\u2115*\n-->  within the finite field F%lu %lu does not have a multiplicative inverse!", common_divisor, *mod_, common_divisor, *mod_, denominator, *mod_, denominator, *mod_, denominator, *mod_, *mod_, *mod_, denominator);
 }
 // error functions for incorrect usage ^^
@@ -28,7 +28,7 @@ void not_a_member_of_an_equivalence_class_from_the_quotient_group()
 int main(int argc, char **argv) { argv_location = &argv; 
     unsigned long multiplicative_group_modulus;   conditional_goodbye(n(n(error_specification(mod_error, n(str_represents_ul(argv[1], &multiplicative_group_modulus, -1)))))); mod_ = &multiplicative_group_modulus;
     int numerator = 1;                            conditional_goodbye(n(n(error_specification(multiplicative_group_element_error, n(str_represents_ul(argv[2], &denominator, -2))))));
-    conditional_goodbye(n(error_message(not_a_member_of_an_equivalence_class_from_the_quotient_group, -3 * !(!((common_divisor = GCD(denominator, multiplicative_group_modulus)) - 1)))));
+    conditional_goodbye(error_message(not_a_member_of_an_equivalence_class_from_the_quotient_group, -3 * !(!((common_divisor = GCD(denominator, multiplicative_group_modulus)) - 1))));
     // process terminal arguments using this genious construction ^^^
 
     fprintf(stdout, "%lu^-%i \u2261 %i / %lu \u2261 ", denominator, NUMERATOR, NUMERATOR, denominator);
