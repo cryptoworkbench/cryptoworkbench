1808cc18d7a8f05238c25a149425089e1e1486fe
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-02 14:06:22 +0100

    wrote artificial intelligence

--- SOURCE_CODE/TOOLS/divide/divide.c
+++ SOURCE_CODE/TOOLS/divide/divide.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include "../../libraries/functional/string.h" // needed for 'str_represents_ul()'
-#include "../../libraries/mathematics/maths.h" // needed for 'mod_inverse()'
+#include "../../libraries/functional/string.h"
+#include "../../libraries/mathematics/maths.h"
 
-const char *MOD_parsing_error = "Please provide as first argument the modulus.";
-const char *denominator_parsing_error = "Please provide as third argument the denominator.";
-const char *numerator_parsing_error = "Please provide as second argument the numerator.";
+void mod_error() { fprintf(stderr, "Please provide as first argument the modulus."); }
+void numerator_error() { fprintf(stderr, "Please provide as second argument the numerator."); }
+void denominator_error() { fprintf(stderr, "Please provide as third argument the denominator."); }
+// error functions ^^^
 
-void mod_error() { fprintf(stderr, MOD_parsing_error); }
-void denominator_error() { fprintf(stderr, denominator_parsing_error); }
-void numerator_error() { fprintf(stderr, numerator_parsing_error); }
+int main(int argc, char **argv) {
+    unsigned long mod;         conditional_goodbye(n(n(error_specification(mod_error, n(str_represents_ul(argv[1], &mod, -1))))));
+    unsigned long numerator;   conditional_goodbye(n(n(error_specification(numerator_error, n(str_represents_ul(argv[2], &numerator, -2))))));
+    unsigned long denominator; conditional_goodbye(n(n(error_specification(denominator_error, n(str_represents_ul(argv[3], &denominator, -3))))));
+    // take in necessary information from terminal arguments ^^^
 
-unsigned long mod; int main(int argc, char **argv) { mod_ = &mod;
-    conditional_goodbye(n(n(error_specification(mod_error, n(str_represents_ul(argv[1], &mod, -1)))))); unsigned long numerator;
-    conditional_goodbye(n(n(error_specification(numerator_error, n(str_represents_ul(argv[2], &numerator, -2)))))); unsigned long denominator;
-    conditional_goodbye(n(n(error_specification(denominator_error, n(str_represents_ul(argv[3], &denominator, -3))))));
-
-    fprintf(stdout, "%lu / %lu \u2261 %lu (mod %lu)\n", numerator, denominator, mod_divide(numerator, denominator), mod);
+    if (numerator >= mod || mod <= denominator) fprintf(stderr, "%lu / %lu \u2261 ", numerator, denominator); mod_ = &mod; numerator = mod_conditional_field_cap(numerator); // *
+    denominator = mod_conditional_field_cap(denominator); fprintf(stdout, "%lu / %lu \u2261 %lu (mod %lu)\n", numerator, denominator, mod_divide(numerator, denominator), mod);
+    // calculate result and display it correctly ^^^ ^^
     return 0;
 }
+// * at line 15: to write conditional clause with closed domain notation NAND approach: 	'   !(numerator < mod && mod > denominator)	'
