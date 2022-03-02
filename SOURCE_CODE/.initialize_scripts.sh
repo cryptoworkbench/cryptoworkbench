be40bb0f9c855261741f3615d81b0efb4b25bfc3
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-02 20:29:49 +0100

    Nice compilation strategies! .. ..

---
+++ SOURCE_CODE/.initialize_scripts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+___failed_builds=0
+successfull_builds=0
+
+cd scripts
+    echo "Initializing scripts:   -->"
+cd subgroup_examplifier_SCRIPT && make subgroup_examplifier_SCRIPT > /dev/null 2>> ../../.script_initialization_errors
+if [ $? -eq 0 ];
+then echo "successfully initialized    'subgroup_examplifier.sh'"; successfull_builds=$(( $successfull_builds + 1 ));
+else echo "                            'subgroup_examplifier.sh' failed to initialize!"; ___failed_builds=$((   $___failed_builds + 1 )); fi; cd ../
+
+cd dh_key_agreement_SCRIPT && make dh_key_agreement_SCRIPT > /dev/null 2>> ../../.script_initialization_errors
+if [ $? -eq 0 ];
+then echo "successfully initialized    'dh_key_agreement.sh'"; successfull_builds=$(( $successfull_builds + 1 ));
+else echo "                            'dh_key_agreement.sh' failed to initialize!"; ___failed_builds=$((   $___failed_builds + 1 )); fi; cd ../
+
+cd prime_factorize_SCRIPT && make prime_factorize_SCRIPT > /dev/null 2>> ../../.script_initialization_errors
+if [ $? -eq 0 ];
+then echo "successfully initialized    'prime_factorize.sh'"; successfull_builds=$(( $successfull_builds + 1 ));
+else echo "                            'prime_factorize.sh' failed to initialize!"; ___failed_builds=$((   $___failed_builds + 1 )); fi; cd ../
+
+echo ""
+echo " /==============================================================================\\"
+echo " | $___failed_builds script(s) failed to initialize! $successfull_builds script(s) were successfully initialized. |"
+echo " \\==============================================================================/"
--- reinitialize_repository.sh
+++ reinitialize_repository.sh
@@ -5,12 +5,7 @@ rm -r applications/ 2> /dev/null;
 if [ $? -eq 0 ];
 then echo "removed 'applications/'";
 else echo "DID NOT HAVE TO REMOVE 'applications/' (good)";
-# else echo "Did not have to remove 'applications/', it seems like somoone already removed it, or you must be initializing using this reinitialization script (nothing wrong with that!).
-# "
 fi;
 
 echo "continuing over at '__initialize_repository.sh'.
 "; ./__initialize_repository.sh
-
-#echo "Handing over control to '__initialize_repository.sh'		(a.k.a. continuing over at '__initialize_repository.sh')
-#"; ./__initialize_repository.sh
