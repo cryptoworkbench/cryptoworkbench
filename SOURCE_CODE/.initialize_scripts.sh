ec29ec46476d8e17a9269208f90ff9e6521aef4b
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-05 21:47:36 +0100

    nice

--- SOURCE_CODE/.initialize_scripts.sh
+++ SOURCE_CODE/.initialize_scripts.sh
@@ -3,23 +3,25 @@
 ___failed_builds=0
 successfull_builds=0
 
+echo "ERRORS FROM THE SHELL SCRIPT '/SOURCE.initialize_scripts.sh':" >> ../.initialization_errors
 cd scripts
     echo "Initializing scripts:   -->"
-cd subgroup_examplifier_SCRIPT && make subgroup_examplifier_SCRIPT > /dev/null 2>> ../../../.initialization_errors.regarding_scripts
+cd subgroup_examplifier_SCRIPT && make subgroup_examplifier_SCRIPT > /dev/null 2>> ../../../.initialization_errors
 if [ $? -eq 0 ];
 then echo "successfully initialized          'subgroup_examplifier.sh'"; successfull_builds=$(( $successfull_builds + 1 ));
 else echo "                                  'subgroup_examplifier.sh'         failed to initialize!"; ___failed_builds=$((   $___failed_builds + 1 )); fi; cd ../
 
-cd dh_key_agreement_SCRIPT && make dh_key_agreement_SCRIPT > /dev/null 2>> ../../../.initialization_errors.regarding_scripts
+cd dh_key_agreement_SCRIPT && make dh_key_agreement_SCRIPT > /dev/null 2>> ../../../.initialization_errors
 if [ $? -eq 0 ];
 then echo "successfully initialized          'dh_key_agreement.sh'"; successfull_builds=$(( $successfull_builds + 1 ));
 else echo "                                  'dh_key_agreement.sh'         failed to initialize!"; ___failed_builds=$((   $___failed_builds + 1 )); fi; cd ../
 
-cd prime_factorize_SCRIPT && make prime_factorize_SCRIPT > /dev/null 2>> ../../../.initialization_errors.regarding_scripts
+cd prime_factorize_SCRIPT && make prime_factorize_SCRIPT > /dev/null 2>> ../../../.initialization_errors
 if [ $? -eq 0 ];
 then echo "successfully initialized          'prime_factorize.sh'"; successfull_builds=$(( $successfull_builds + 1 ));
 else echo "                                  'prime_factorize.sh'        failed to initialize!"; ___failed_builds=$((   $___failed_builds + 1 )); fi; cd ../
 
+cd ../..; echo "" >> .initialization_errors; echo "" >> .initialization_errors;
 echo ""
 echo " /==============================================================================\\"
 echo " | $___failed_builds script(s) failed to initialize! $successfull_builds script(s) were successfully initialized. |"
--- __initialize_repository.sh
+++ __initialize_repository.sh
@@ -1,8 +1,10 @@
 # sort using ls -'Xla' to sort alphabetically on the error file's extension
 #!/bin/bash
 
-mkdir applications/ > /dev/null 2>> .initialization_errors.regarding_mkdir; if [ $? -eq 0 ]; then echo "Created target directory 'applications/'"; else echo "Target directory 'applications' already existed!"; fi
-mkdir applications/ARCHIVE > /dev/null 2>> .initialization_errors.regarding_mkdir; if [ $? -eq 0 ]; then echo "Created target directory 'applications/ARCHIVE'"; else echo "Target directory 'applications/ARCHIVE' already existed!"; fi
+echo "ERRORS FROM THE SHELL SCRIPT '/.__initialize.sh':" >> .initialization_errors
+mkdir applications/ > /dev/null 2>> .initialization_errors; if [ $? -eq 0 ]; then echo "Created target directory 'applications/'"; else echo "Target directory 'applications' already existed!"; fi
+mkdir applications/ARCHIVE > /dev/null 2>> .initialization_errors; if [ $? -eq 0 ]; then echo "Created target directory 'applications/ARCHIVE'"; else echo "Target directory 'applications/ARCHIVE' already existed!"; fi
+echo "" >> .initialization_errors; echo "" >> .initialization_errors;
 
 cd SOURCE_CODE/
 echo ""; ./.initialize_applications.sh;
