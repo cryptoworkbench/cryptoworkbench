b31491ef5536e9f824bb8c201cf38349577c379b
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-06 18:49:21 +0100

    fixed jacobi_symbol but do not know how to continue.

--- SOURCE_CODE/applications/jacobi_symbol/jacobi_symbol.c
+++ SOURCE_CODE/applications/jacobi_symbol/jacobi_symbol.c
@@ -24,17 +24,17 @@
 #include "../../libraries/mathematics/factorization_methods.h"
 
 // Legendre symbol for composite numerator and odd prime denominator
-int _legendre_symbol(unsigned long N, unsigned long q, int multiplicative_accumulator, int index, struct _PRIME_FACTORIZATION *prime_factorization) {
+int _legendre_symbol(unsigned long N, unsigned long D, int multiplicative_accumulator, int index, struct _PRIME_FACTORIZATION *prime_factorization) {
     if (index == prime_factorization->number_of_distinct_prime_factors) { PRIME_FACTORIZATION_free(prime_factorization); return multiplicative_accumulator; }
-    int i = eulers_criterion(prime_factorization->prime_factor[index], q); if (!(i + 1 || prime_factorization->log[index] % 2)) i = 1;
-    return _legendre_symbol(N, q, multiplicative_accumulator * i, index + 1, prime_factorization);
+    int i = eulers_criterion(prime_factorization->prime_factor[index], D); if (!(i + 1 || prime_factorization->log[index] % 2)) i = 1;
+    return _legendre_symbol(N, D, multiplicative_accumulator * i, index + 1, prime_factorization);
 }
 
-// Same as above but adds support for even prime and multiplicative identity 
-int legendre_symbol(unsigned long N, unsigned long q) {
-    if (N == 1) return 1; // 1 is always a quadratic residue in groups with mod > 1
-    else if (N == 2) { int i = q % 8; if (i == 1 || i == 7) return 1; else if (i == 3 || i == 5) return -1; }
-    return _legendre_symbol(N, q, 1, 0, PRIME_FACTORIZATION(N));
+// Same as above but adds support for even prime and 1
+int legendre_symbol(unsigned long N, unsigned long D) {
+    if (N == 1) return 1; // 1 will always be a quadratic residue
+    else if (N == 2) { int i = D % 8; if (i == 1 || i == 7) return 1; else if (i == 3 || i == 5) return -1; }
+    return _legendre_symbol(N, D, 1, 0, PRIME_FACTORIZATION(N));
 }
 
 // Attempts to generalize the above using factorization for the denominator
@@ -50,29 +50,27 @@ int jacobi_symbol(unsigned long p, unsigned long C) {
     return m;
 }
 
-unsigned long N, q;
+unsigned long D; // Denominator <--
+unsigned long N; // Numerator <--
 
 // void coprime_error() { fprintf(stderr, "gcd(%s, %s) != 1: %s is not an element from \u2115%s*.", (*argv_location)[i], (*argv_location)[1], (*argv_location)[i], (*argv_location)[1]); }
-void coprime_fail() { fprintf(stderr, "%s is not an element from \u2115%s*.", (*argv_location)[1], (*argv_location)[2]); }
-void denominator_fail() { fprintf(stderr, "Please provide as second argument the denominator to the Jacobi symbol."); }
-void numerator_fail() { fprintf(stderr, "Please provide as first argument the numerator to the Jacobi symbol."); }
+void coprime_fail() { fprintf(stderr, "%s is neither an element from \u2115/%s\u2115* nor from \u2115%s*", (*argv_location)[2], (*argv_location)[1], (*argv_location)[1]); }
+void denominator_fail() { fprintf(stderr, "Please specify in the first argument a multiplicative group specification (goes as denominator into the Jacobi symbol)."); }
+void numerator_fail() { fprintf(stderr, "Please specify in the second argument an element from \u2115%s*", (*argv_location)[1]); }
 
 int main(int argc, char **argv) {
-    conditional_goodbye(n(n(error_specification(numerator_fail, n(ul_parse_str(argv[1], &N, -1))))));
-    conditional_goodbye(n(n(error_specification(denominator_fail, n(ul_parse_str(argv[2], &q, -1)))))); argv_location = &argv;
-    conditional_goodbye(n(n(error_message(coprime_fail, -2 * ( !coprime(N, q) )))));
-    if (N > q) { N %= q; fprintf(stdout, "Reduced N modulus q.\n"); }
+    conditional_goodbye(n(n(error_specification(denominator_fail, n(ul_parse_str(argv[1], &D, -1)))))); argv_location = &argv;
+    conditional_goodbye(n(n(error_specification(numerator_fail, n(ul_parse_str(argv[2], &N, -1))))));
+    conditional_goodbye(n(n(error_message(coprime_fail, -2 * !coprime(N, D))))); if (N > D) N %= D;
     // verify arguments validity ^^^
 
-    _preferred_factorization_engine = factorization_method_retrieve(argv[2]);
+    _preferred_factorization_engine = factorization_method_retrieve(argv[3]);
 
-    fprintf(stdout, "Legendre's symbol for N over q ( N / q ) = %s over %s = ( %s / %s ) = %i\n", argv[1], argv[2], argv[1], argv[2], legendre_symbol(N, q)); // PROGRAM APPROACH <--
-    /*
     unsigned long ans;
-    if (!primality_test_based_on_preferred_factorization_engine(q)) ans = jacobi_symbol(N, q);
-    else ans = legendre_symbol(N, q);
-    if (ans == 1) fprintf(stdout, "%lu is a quadratic residue mod %lu\n", N, q); // BASH SCRIPT APPROACH <--
-    else if (ans == - 1) fprintf(stdout, "%lu is not a quadratic residue mod %lu\n", N, q); // BASH SCRIPT APPROACH <--
-    */
+    if (!primality_test_based_on_preferred_factorization_engine(D)) ans = jacobi_symbol(N, D);
+    else ans = legendre_symbol(N, D);
+    if (ans == 1) fprintf(stdout, "%lu is a quadratic residue mod %lu\n", N, D); // BASH SCRIPT APPROACH <--
+    else if (ans == - 1) fprintf(stdout, "%lu is not a quadratic residue mod %lu\n", N, D); // BASH SCRIPT APPROACH <--
+    // fprintf(stdout, "Legendre's symbol for N over D ( N / D ) = %s over %s = ( %s / %s ) = %i\n", argv[1], argv[2], argv[1], argv[2], legendre_symbol(N, D)); // PROGRAM APPROACH <--
     return 0;
 }
