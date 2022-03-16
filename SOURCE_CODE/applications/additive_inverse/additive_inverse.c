6921b65a302e3ad2ca20f839a60664c56b5cf5cc
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-16 17:22:27 +0100

    fixed additive_inverse.c

--- SOURCE_CODE/applications/additive_inverse/additive_inverse.c
+++ SOURCE_CODE/applications/additive_inverse/additive_inverse.c
@@ -9,11 +9,12 @@ void mod_failed_to_parse() { fprintf(stderr, "Please provide as first argument t
 void _id_failed_to_parse() { fprintf(stderr, "Please provide as second argument the number to take the additive inverse of."); }
 
 int main(int argc, char **argv) {
-    unsigned long mod;                        conditional_goodbye(n(n(error_specification(mod_failed_to_parse, n(ul_parse_str(                       &mod, argv[1], -1))))));
-    unsigned long inverse_of_additve_inverse; conditional_goodbye(n(n(error_specification(_id_failed_to_parse, n(ul_parse_str(&inverse_of_additve_inverse, argv[2], -2))))));
+    unsigned long mod = 0;                        conditional_goodbye(n(n(error_specification(mod_failed_to_parse, n(ul_parse_str(                       &mod, argv[1], -1))))));
+    unsigned long inverse_of_additive_inverse = 0; conditional_goodbye(n(n(error_specification(_id_failed_to_parse, n(ul_parse_str(&inverse_of_additive_inverse, argv[2], -2)))))); mod_ = &mod;
 
-    mod_ = &mod; unsigned long additive_inverse = mod_inverse(inverse_of_additve_inverse);
-    fprintf(stdout, "x - %lu \u2261 x + %lu (mod %lu)\n\n", inverse_of_additve_inverse, additive_inverse, mod);
-    fprintf(stdout, "The additive inverse of %lu under modulus %lu arithmetic is %lu.\n", inverse_of_additve_inverse, mod, additive_inverse);
+    fprintf(stdout, "x - %lu \u2261 ", inverse_of_additive_inverse);
+    if (inverse_of_additive_inverse != mod_conditional_cap(inverse_of_additive_inverse)) fprintf(stdout, "x - %lu \u2261 ", inverse_of_additive_inverse = mod_conditional_cap(inverse_of_additive_inverse));
+    unsigned long additive_inverse; fprintf(stdout, "x + %lu	(mod %lu)\n\n", additive_inverse = mod_inverse(inverse_of_additive_inverse), *mod_);
+    fprintf(stdout, "The additive inverse of %lu under modulus %lu arithmetic is %lu.\n", inverse_of_additive_inverse, mod, additive_inverse);
     return 0;
 }
