c4891f28f145a07f0e8bb0cc2611a8add01def7a
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-18 17:32:01 +0100

    removed makefile from mathematics/.

--- SOURCE_CODE/applications/prime_table_exporter/prime_table_exporter.c
+++ SOURCE_CODE/applications/prime_table_exporter/prime_table_exporter.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../libraries/functional/string.h"
-#include "../../libraries/mathematics/math.extended.h"
+#include "../../libraries/mathematics/math.basic.h"
 
 unsigned long limit;
 
--- SOURCE_CODE/applications/random_numbers/random_numbers.c
+++ SOURCE_CODE/applications/random_numbers/random_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../libraries/functional/string.h"
-#include "../../libraries/mathematics/math.extended.h"
+#include "../../libraries/mathematics/math.basic.h"
 // library inclusions ^
 
 unsigned long upper_bound, quantity;
