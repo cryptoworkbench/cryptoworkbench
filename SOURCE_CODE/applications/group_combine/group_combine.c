ee8bf1bf75cda9080012f266e31f518ebc700565
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-19 11:55:25 +0100

    .

--- SOURCE_CODE/applications/group_combine/group_combine.c
+++ SOURCE_CODE/applications/group_combine/group_combine.c
@@ -13,11 +13,11 @@ void ___array_failed_to_parse() { fprintf(stderr, "This function requires at lea
 void sp_array_failed_to_parse() {
     fprintf(stderr, "Please specify from the second argument onward numbers to %s within the finite field F%s.", id_as_verb(), (*argv_ptr)[1]);
 }
-void ______id_failed_to_parse() { fprintf(stderr, "Please specify as second argument the identity element of the group whose subgroups to examplify."); list_plausable_group_identity_descriptions(2); }
-void _____mod_failed_to_parse() { fprintf(stderr, "Please specify as first argument the modulus of the group operation (0 to act upon the infinite group of integers)."); }
+void _id_failed_to_parse() { fprintf(stderr, "Please specify as second argument the identity element of the group whose subgroups to examplify."); list_plausable_group_identity_descriptions(2); }
+void mod_failed_to_parse() { fprintf(stderr, "Please specify as first argument the modulus of the group operation (0 to act upon the infinite group of integers)."); }
 
 int main(int argc, char **argv) { i_ = &i; array_size_ = &function_arg_count; mod = id = ADDITIVE_IDENTITY; mod_ = &mod; id_ = &id; operation = &group_oper; argv_ptr = &argv;
-    group_parse(_____mod_failed_to_parse, ______id_failed_to_parse, 1);
+    group_parse(mod_failed_to_parse, _id_failed_to_parse, 1);
     // process first two arguments ^^
 
     conditional_goodbye(n(n(error_specification(sp_array_failed_to_parse, n(n(error_message(___array_failed_to_parse, (2 > (function_arg_count = argc - STARTING_INDEX_OF_ARGS_LIST)) * - STARTING_INDEX_OF_ARGS_LIST)))))));
@@ -29,7 +29,11 @@ int main(int argc, char **argv) { i_ = &i; array_size_ = &function_arg_count; mo
     do {ans = mod_group_operation(ans, LOOP_array[i]);
 	fprintf(stdout, "%lu", LOOP_array[i]);
 	if (i + 1 == function_arg_count) break; i++; fprintf(stdout, " %s ", symb);
-    } while (1); fprintf(stdout, " \u2261 %lu", ans); if (mod) fprintf(stdout, "	(mod %s)", argv[1]); fprintf(stdout, "\n");
-    free(LOOP_array);
-    return 0;
+    } while (1);
+
+    if (mod) fprintf(stdout, " \u2261 %lu	(mod %s)", ans, argv[1]); else fprintf(stdout, " = %lu", ans); fprintf(stdout, "\n");
+    // display result ^^
+
+    free(LOOP_array); return 0;
+    // finish cleanly ^^
 }
