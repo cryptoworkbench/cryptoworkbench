bab6172fa88740c76aaff30e205c5c2307b49e47
cryptoworkbench <nj.vanhoof@gmail.com>
2022-03-19 10:18:15 +0100

    .

--- SOURCE_CODE/libraries/functional/string.extended.c
+++ SOURCE_CODE/libraries/functional/string.extended.c
@@ -3,10 +3,11 @@
 char *width_str_from_ul(unsigned long ul)
 { return _str_from_ul(ul, *width_); }
 
-void LOOP_ul_parse_str(_error_function error_function, ui start)
+unsigned long *LOOP_ul_parse_str(_error_function error_function, ui start)
 {
-    arguments_array = UL_array_of_SIZE(*array_size_);
-    for (*i_ = 0; *i_ < *array_size_; (*i_)++) conditional_goodbye(n(n(error_specification(error_function, n(_ul_parse_str(*i_ + arguments_array, (*argv_ptr)[start + *i_], -(start + 1 + *i_)))))));
+    ul *ret_val = UL_array_of_SIZE(*array_size_);
+    for (*i_ = 0; *i_ < *array_size_; (*i_)++) conditional_goodbye(n(n(error_specification(error_function, n(_ul_parse_str(ret_val + *i_, (*argv_ptr)[start + *i_], -(start + 1 + *i_)))))));
+    return ret_val;
 }
 
 void LOOP_ul_parsed_strs()
